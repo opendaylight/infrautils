7d52a173c57550814ddb5bf20364901b5cf545c0
Ravit Peretz <ravit.peretz@hpe.com>
2016-07-31 13:38:32 +0300

    Remove internal parent, checkstyle etc. and fixing violations
    Change-Id: Id9e1aee6d464df340f38d8ce6e7033fb0830e6a8
    Signed-off-by: Ravit Peretz <ravit.peretz@hpe.com>

--- async/impl/src/main/java/org/opendaylight/infrautils/async/impl/ThreadPoolExecutorFactory.java
+++ async/impl/src/main/java/org/opendaylight/infrautils/async/impl/ThreadPoolExecutorFactory.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2016 Hewlett Packard Enterprise, Co. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.infrautils.async.impl;
 
 import java.util.concurrent.Callable;
