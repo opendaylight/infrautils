e7a7c6c751846dc73ec1052c3eb33179f0c908bb
Michael Vorburger <vorburger@redhat.com>
2016-08-23 15:59:43 +0200

    org.opendaylight.infrautils.inject
    Change-Id: I88313940370e6cbef30bd471b2cda8238be5beb8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ inject/inject/src/main/java/org/opendaylight/infrautils/inject/SingletonWithLifecycle.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject;
+
+import javax.inject.Scope;
+import javax.inject.Singleton;
+
+/**
+ * A Singleton AbstractLifecycle.
+ *
+ * <p>In ODL, most wired objects ("beans") are {@link Singleton}, and it therefore
+ * makes sense to have this class and let both exposed Services and wired
+ * objects internal to bundles extend this.
+ *
+ * <p>Future use of DI in ODL may introduce additional non-{@link Singleton} {@link Scope}s.
+ *
+ * @author Michael Vorburger
+ */
+@Singleton
+public class SingletonWithLifecycle extends Lifecycle {
+}
