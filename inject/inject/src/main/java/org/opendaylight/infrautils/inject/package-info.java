56b2a4b165c7ca0cbcb3eb386268dd2587b32df1
Michael Vorburger <vorburger@redhat.com>
2016-08-23 15:59:43 +0200

    org.opendaylight.infrautils.inject
    Change-Id: I88313940370e6cbef30bd471b2cda8238be5beb8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ inject/inject/src/main/java/org/opendaylight/infrautils/inject/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/**
+ * The interfaces and classes in this package are "for convenience". Technically
+ * you do NOT have to implement or extend any of these to work with Dependency
+ * Injection - and could instead just apply these annotations to a class
+ * yourself etc. But defining this once and encouraging projects to use this
+ * establishes uniformity, which also helps other reading your code to recognize
+ * a class as being of the respective kind.
+ */
+package org.opendaylight.infrautils.inject;
