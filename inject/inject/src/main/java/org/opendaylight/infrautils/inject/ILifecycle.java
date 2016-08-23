0a84df03159b900cffae8498823c9d4076880c1e
Michael Vorburger <vorburger@redhat.com>
2016-08-23 15:59:43 +0200

    org.opendaylight.infrautils.inject
    Change-Id: I88313940370e6cbef30bd471b2cda8238be5beb8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ inject/inject/src/main/java/org/opendaylight/infrautils/inject/ILifecycle.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Something which {@link #start()}s and {@link #stop()}s.
+ *
+ * <p>Annotated so that Dependency Injection Frameworks (whichever) automatically call these methods during wiring.
+ *
+ * @see Lifecycle
+ * @see SingletonWithLifecycle
+ *
+ * @author Michael Vorburger
+ */
+public interface ILifecycle {
+
+    @PostConstruct
+    void start() throws ModuleSetupRuntimeException;
+
+    @PreDestroy
+    void stop() throws ModuleSetupRuntimeException;
+
+    boolean isRunning();
+
+}
