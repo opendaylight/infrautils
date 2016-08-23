2db77b0fdaa268deae7c7423c8b7712b42efba95
Michael Vorburger <vorburger@redhat.com>
2016-08-23 15:59:43 +0200

    @Inject convenience helper (org.opendaylight.infrautils.inject)
    Introduces the Lifecycle interrface with the LifecycleSupport and
    SingletonWithLifecycle convenience base classes, with a
    ModuleSetupRuntimeException, to standardize the use of @Inject (in
    @Singleton) in the ODL code base for projects using the
    blueprint-maven-plugin to generate wiring XML from these Java
    annotations instead of hand writing it.
    
    Change-Id: I88313940370e6cbef30bd471b2cda8238be5beb8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ inject/inject/src/main/java/org/opendaylight/infrautils/inject/AbstractLifecycle.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject;
+
+import java.util.concurrent.atomic.AtomicReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Support class for {@link Lifecycle}. Provides a convenient base
+ * implementation including correct thread safety, exception handling and check
+ * for accidental unnecessary re-start &amp; stop. Subclasses must implement
+ * {@link #start()} &amp; {@link #stop()}.
+ *
+ * @author Michael Vorburger (with guidance re. AtomicReference from Tom Pantelis)
+ */
+public abstract class AbstractLifecycle implements Lifecycle {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractLifecycle.class);
+
+    private enum State {
+        STARTED, STOPPED
+    }
+
+    private AtomicReference<State> state = new AtomicReference<>(State.STOPPED);
+
+    protected abstract void start() throws Exception;
+
+    protected abstract void stop() throws Exception;
+
+    /**
+     * Please override startWithException() instead of this (here intentionally final) method.
+     */
+    @Override
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    public final void init() throws ModuleSetupRuntimeException {
+        if (state.compareAndSet(State.STOPPED, State.STARTED)) {
+            try {
+                start();
+            } catch (Exception e) {
+                throw new ModuleSetupRuntimeException(e);
+            }
+        } else {
+            LOG.warn("Lifecycled object already started; ignoring start()");
+        }
+    }
+
+    /**
+     * Please override stopWithException() instead of this (here intentionally final) method.
+     */
+    @Override
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    public final void destroy() throws ModuleSetupRuntimeException {
+        if (state.compareAndSet(State.STARTED, State.STOPPED)) {
+            try {
+                stop();
+            } catch (Exception e) {
+                throw new ModuleSetupRuntimeException(e);
+            }
+        } else {
+            LOG.warn("Lifecycled object already stopped; ignoring stop()");
+        }
+    }
+
+    @Override
+    public boolean isRunning() {
+        return state.get() == State.STARTED;
+    }
+
+}
