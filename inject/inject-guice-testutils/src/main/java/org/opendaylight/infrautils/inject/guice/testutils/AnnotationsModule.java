319bd4b699c48edb8c380caeaec7d80806b70798
Michael Vorburger <vorburger@redhat.com>
2016-12-02 12:59:30 +0100

    (WIP) Caches
    see (YT-V;asciinema)
    
    Change-Id: Ie46f481077283524789212ee48b059fdb8988a62
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ inject/inject-guice-testutils/src/main/java/org/opendaylight/infrautils/inject/guice/testutils/AnnotationsModule.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject.guice.testutils;
+
+import com.google.inject.AbstractModule;
+import com.mycila.guice.ext.closeable.CloseableModule;
+import com.mycila.guice.ext.jsr250.Jsr250Module;
+
+/**
+ * Guice module with built-in Mycila Guice Extensions for JSR-250 &amp;
+ * Closeable support for {@literal @}PreDestroy &amp; {@literal @}PostConstruct.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class AnnotationsModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        install(new CloseableModule());
+        install(new Jsr250Module());
+    }
+
+}
