f1be77374ccae9f17eb61a5174c8263a81f006b6
Michael Vorburger <vorburger@redhat.com>
2016-12-02 12:59:30 +0100

    (WIP) Caches
    see (YT-V;asciinema)
    
    Change-Id: Ie46f481077283524789212ee48b059fdb8988a62
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ caches/api/src/main/java/org/opendaylight/infrautils/caches/ops/CachePolicy.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.caches.ops;
+
+import static org.immutables.value.Value.Style.ImplementationVisibility.PRIVATE;
+
+import org.immutables.value.Value;
+import org.immutables.value.Value.Default;
+import org.opendaylight.infrautils.caches.Cache;
+import org.opendaylight.infrautils.caches.CheckedCache;
+
+/**
+ * Policy of a {@link Cache} (or {@link CheckedCache}).
+ *
+ * @author Michael Vorburger.ch
+ */
+@Value.Immutable
+@Value.Style(visibility = PRIVATE, strictBuilder = false)
+public interface CachePolicy {
+
+    @Default default boolean statsEnabled() {
+        return false;
+    }
+
+    @Default default long maxEntries() {
+        return 743; // arbitrary
+    }
+
+    // TODO Map<String,String> extensions();
+    //     TODO how to set?  Builder supported how? or split:
+    //         List<String> extensions();
+    //         String extension(String name);
+}
