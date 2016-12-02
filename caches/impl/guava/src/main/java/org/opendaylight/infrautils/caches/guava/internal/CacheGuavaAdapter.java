ade9dae203a2653c1bb642ad75b786259c6cbbfb
Michael Vorburger <vorburger@redhat.com>
2016-12-02 12:59:30 +0100

    Bug 8300: Caches v1.0 incl. CLI commands (see demo video)
    see https://www.youtube.com/watch?v=h4HOSRN2aFc
    
    The main interface APIs are CacheProvider, Cache and CacheFunction.
    
    The SampleServiceWithCachingImpl class illustrates the intended usage.
    
    Actual cache implementations are pluggable; Guava for now, more later.
    
    Change-Id: Ie46f481077283524789212ee48b059fdb8988a62
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ caches/impl/guava/src/main/java/org/opendaylight/infrautils/caches/guava/internal/CacheGuavaAdapter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.caches.guava.internal;
+
+import com.google.common.base.Throwables;
+import com.google.common.cache.CacheLoader.InvalidCacheLoadException;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+import org.opendaylight.infrautils.caches.BadCacheFunctionRuntimeException;
+import org.opendaylight.infrautils.caches.Cache;
+import org.opendaylight.infrautils.caches.CacheConfig;
+import org.opendaylight.infrautils.caches.CachePolicy;
+
+/**
+ * Adapts {@link Cache} to Guava.
+ *
+ * @author Michael Vorburger.ch
+ */
+final class CacheGuavaAdapter<K, V> extends GuavaBaseCacheAdapter<K, V> implements Cache<K, V> {
+
+    CacheGuavaAdapter(CacheConfig<K, V> config, CachePolicy initialPolicy,
+            Function<CachePolicy, LoadingCache<K, V>> policyToCache) {
+        super(config, initialPolicy, policyToCache);
+    }
+
+    @Override
+    public V get(K key) {
+        try {
+            return guavaCache().getUnchecked(key);
+        } catch (UncheckedExecutionException e) {
+            throw Throwables.propagate(e.getCause());
+        } catch (InvalidCacheLoadException e) {
+            throw new BadCacheFunctionRuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Map<K, V> get(Iterable<? extends K> keys) {
+        try {
+            return guavaCache().getAll(keys);
+        } catch (UncheckedExecutionException e) {
+            throw Throwables.propagate(e.getCause());
+        } catch (InvalidCacheLoadException e) {
+            throw new BadCacheFunctionRuntimeException(e.getMessage());
+        } catch (ExecutionException e) {
+            // This normally should never happen here, because according to Guava Cache's doc,
+            // an ExecutionException is thrown by getAll when the its CacheLoader (thus our
+            // CacheFunction) throws a checked exception - but our CacheFunction never can,
+            // according to its signature - that's what the CheckedCacheFunction and
+            // CheckedCacheGuavaAdapter are for... if this happens, something is wrong.
+            // NB This is very different in CheckedCacheGuavaAdapter, where we do expect it!
+            throw new BadCacheFunctionRuntimeException("CacheFunction checked exception", e);
+        }
+    }
+
+}
---
+++ caches/impl/noop/src/main/java/org/opendaylight/infrautils/caches/noop/internal/NoopCache.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.caches.noop.internal;
+
+import java.util.Map;
+import org.opendaylight.infrautils.caches.Cache;
+import org.opendaylight.infrautils.caches.CacheConfig;
+import org.opendaylight.infrautils.caches.CacheFunction;
+import org.opendaylight.infrautils.caches.CacheManager;
+
+/**
+ * No Operation ("NOOP") implementation of Cache.
+ *
+ * @author Michael Vorburger.ch
+ */
+final class NoopCache<K, V> implements Cache<K,V> {
+
+    private final CacheConfig<K, V> config;
+    private final CacheFunction<K, V> function;
+    private final CacheManager manager;
+
+    NoopCache(CacheConfig<K, V> config) {
+        this.config = config;
+        this.manager = new NoopCacheManager(config);
+        this.function = config.cacheFunction();
+    }
+
+    @Override
+    public V get(K key) {
+        return function.get(key);
+    }
+
+    @Override
+    public Map<K, V> get(Iterable<? extends K> keys) {
+        return function.get(keys);
+    }
+
+    @Override
+    public void evict(K key) {
+        return;
+    }
+
+    @Override
+    public CacheManager getManager() {
+        return manager;
+    }
+
+    @Override
+    public void close() throws Exception {
+        // Nothing to do.
+    }
+
+    @Override
+    public String toString() {
+        return "NoopCache{config=" + config + "}";
+    }
+}
