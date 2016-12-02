cce95f63ab66fa4b68073a08e1449764db347f78
Michael Vorburger <vorburger@redhat.com>
2016-12-02 12:59:30 +0100

    (WIP) Caches
    see (YT-V;asciinema)
    
    Change-Id: Ie46f481077283524789212ee48b059fdb8988a62
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ caches/impl/noop/src/main/java/org/opendaylight/infrautils/caches/noop/internal/NoopCachePolicy.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.caches.noop.internal;
+
+import org.opendaylight.infrautils.caches.ops.CachePolicy;
+
+/**
+ * No Operation ("NOOP") implementation of CachePolicy.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class NoopCachePolicy implements CachePolicy {
+
+    static final CachePolicy INSTANCE = new NoopCachePolicy();
+
+    @Override
+    public long maxEntries() {
+        return 0;
+    }
+
+}
