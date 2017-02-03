af9187adda509ad2d4ea0284fc36bf10d456bf45
Faseela K <faseela.k@ericsson.com>
2017-02-03 23:12:27 +0530

    Initial structure for diag-status module
    Change-Id: I4665b53040da4ffc438b17182fe03aa111717cd1
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/shell/src/main/java/org/opendaylight/infrautils/diagstatus/shell/DiagStatusCommand.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus.shell;
+
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.commands.Option;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * CLI for showing remote service status.
+ *
+ * @author Faseela K
+ */
+@Command(scope = "diagstatus", name = "list", description = "getRemoteServicesStatus- ")
+public class DiagStatusCommand extends OsgiCommandSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DiagStatusCommand.class);
+
+    @Option(name = "-n", aliases = {"--node"}, required = false, multiValued = false)
+    String nip;
+    @Option(name = "-a", aliases = {"--all"}, required = false, multiValued = false)
+    String all;
+    @Option(name = "-h", aliases = {"--help"}, required = false, multiValued = false)
+    String help;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        //TODO will come in subsequent patches
+        return null;
+    }
+}
