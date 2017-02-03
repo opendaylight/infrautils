6e9505ef4e021c6df6cda436037c3a0533a852e2
Faseela K <faseela.k@ericsson.com>
2017-02-03 23:12:27 +0530

    Initial structure for diag-status module
    Change-Id: I4665b53040da4ffc438b17182fe03aa111717cd1
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/diagstatus-impl/src/test/java/org/opendaylight/infrautils/diagstatus/test/DiagStatusTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DiagStatusTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testDiagStausAPI() throws Exception {
+
+    }
+}
