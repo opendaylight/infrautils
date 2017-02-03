9b4a170f514f8aa4885dc2ffb4add6d5aeacfebd
Faseela K <faseela.k@ericsson.com>
2017-02-03 23:12:27 +0530

    Initial structure for diag-status module
    Change-Id: I4665b53040da4ffc438b17182fe03aa111717cd1
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/api/src/main/java/org/opendaylight/infrautils/diagstatus/ServiceDescriptor.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.infrautils.diagstatus;
+
+import com.google.common.base.MoreObjects;
+import java.time.Instant;
+import java.util.Objects;
+import org.opendaylight.yangtools.util.EvenMoreObjects;
+
+/**
+ * Details of a registered service.
+ *
+ * @author Faseela K
+ */
+public class ServiceDescriptor {
+
+    private final String moduleServiceName;
+    private final ServiceState serviceState;
+    private final Instant timestamp;
+    private final String statusDesc; // In case of ERROR state specific error message to aid troubleshooting can be
+                                     // provided  by monitored service
+
+    public ServiceDescriptor(String moduleServiceName, ServiceState svcState, String statusDesc) {
+        this.moduleServiceName = moduleServiceName;
+        this.serviceState = svcState;
+        this.statusDesc = statusDesc;
+        this.timestamp = Instant.now();
+    }
+
+    public String getModuleServiceName() {
+        return moduleServiceName;
+    }
+
+    public ServiceState getServiceState() {
+        return serviceState;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public String getStatusDesc() {
+        return statusDesc;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("serviceName", getModuleServiceName()).add("serviceState",
+            getServiceState()).add("statusDesc", getStatusDesc()).add("timeStamp", getTimestamp()).toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EvenMoreObjects.equalsHelper(this, obj,
+            (self, other) -> Objects.equals(self.getModuleServiceName(), other.getModuleServiceName())
+                && Objects.equals(self.getServiceState(), other.getServiceState())
+                && Objects.equals(self.getStatusDesc(), other.getStatusDesc())
+                && Objects.equals(self.getTimestamp(), other.getTimestamp()));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getModuleServiceName(), getServiceState(), getStatusDesc(), getTimestamp());
+    }
+}
