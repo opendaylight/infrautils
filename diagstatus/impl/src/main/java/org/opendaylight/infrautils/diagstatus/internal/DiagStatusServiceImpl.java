4b78d0da7e9aec4f070e6cc02705acaf5b55d954
Faseela K <faseela.k@ericsson.com>
2017-02-03 23:12:27 +0530

    Initial structure for diag-status module
    Change-Id: I4665b53040da4ffc438b17182fe03aa111717cd1
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/impl/src/main/java/org/opendaylight/infrautils/diagstatus/internal/DiagStatusServiceImpl.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.infrautils.diagstatus.internal;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.infrautils.diagstatus.DiagStatusService;
+import org.opendaylight.infrautils.diagstatus.ServiceDescriptor;
+import org.opendaylight.infrautils.diagstatus.ServiceState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * DiagStatusServiceImpl is the core class having the functionality for loading the configured services
+ * and aggregating the status of the same.
+ * @author Faseela K
+ */
+@Singleton
+public class DiagStatusServiceImpl implements DiagStatusService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DiagStatusServiceImpl.class);
+    public static final String JMX_OBJECT_NAME = "org.opendaylight.infrautils.diagstatus:type=SvcStatus";
+
+    @Inject
+    public DiagStatusServiceImpl() {
+        LOG.info("{} initialized", getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void start() {
+        LOG.info("{} start", getClass().getSimpleName());
+        initializeStatusMonService();
+    }
+
+    public void initializeStatusMonService() {
+        MBeanService.registerServerMBean(this, JMX_OBJECT_NAME);
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        MBeanService.unregisterServerMBean(this, JMX_OBJECT_NAME);
+        LOG.info("{} close", getClass().getSimpleName());
+    }
+
+    @Override
+    public CompletableFuture<Void> register(String service) {
+        //TODO will come in subsequent patches
+        return null;
+    }
+
+    @Override
+    public void report(String service, ServiceState serviceState) {
+        //TODO will come in subsequent patches
+    }
+
+    @Override
+    public ServiceDescriptor getServiceDescriptor(String serviceIdentifier) {
+        //TODO will come in subsequent patches
+        return null;
+    }
+
+    @Override
+    public List<ServiceDescriptor> getAllServiceDescriptors() {
+        //TODO will come in subsequent patches
+        return null;
+    }
+}
