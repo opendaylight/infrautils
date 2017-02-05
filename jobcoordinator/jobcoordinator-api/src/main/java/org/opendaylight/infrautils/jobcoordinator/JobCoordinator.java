67bc1838a54b2ad03cf048e19b44f81c2c587187
Yakir Dorani <yakir.dorani@hpe.com>
2017-02-05 13:37:48 +0200

    Move DataStoreJobCoordinator from genius to infrautils
    Change-Id: Iacbf938b9bede086d754e5494d5b50fdbe28a705
    Signed-off-by: Yakir Dorani <yakir.dorani@hpe.com>

---
+++ jobcoordinator/jobcoordinator-api/src/main/java/org/opendaylight/infrautils/jobcoordinator/JobCoordinator.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2017 Hewlett-Packard Enterprise and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.jobcoordinator;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * This interface defines methods for a JobCoordinator which enables executing
+ * jobs in a parallel/sequential fashion based on their keys.
+ */
+public interface JobCoordinator {
+
+    /**
+     * Enqueues a job.
+     *
+     * @param key
+     *            The job's key. Jobs with the same key are run sequentially.
+     *            Jobs with different keys are run in parallel.
+     * @param mainWorker
+     *            The task that runs for the job.
+     */
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker);
+
+    /**
+     * Enqueues a job with a rollback task.
+     *
+     * @param rollbackWorker
+     *            The rollback task which runs in case the job's main task
+     *            fails.
+     * @see JobCoordinator#enqueueJob(String, Callable)
+     */
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker,
+            RollbackCallable rollbackWorker);
+
+    /**
+     * Enqueues a job with max retries. In case the job's main task fails, it
+     * will be retried until it succeeds or the specified maximum number of
+     * retries has been reached.
+     *
+     * @param maxRetries
+     *            The maximum number of retries for the job's main task until it
+     *            succeeds.
+     * @see JobCoordinator#enqueueJob(String, Callable)
+     */
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, int maxRetries);
+
+    /**
+     * Enqueues a job with a rollback task and max retries.
+     *
+     * @param rollbackWorker
+     *            The rollback task which runs in case the job's main task
+     *            fails.
+     * @param maxRetries
+     *            The maximum number of retries for the job's main task until it
+     *            succeeds.
+     * @see JobCoordinator#enqueueJob(String, Callable, RollbackCallable)
+     * @see JobCoordinator#enqueueJob(String, Callable, int)
+     */
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker,
+            RollbackCallable rollbackWorker, int maxRetries);
+
+    /**
+     * Returns the cleared task count.
+     */
+    long getClearedTaskCount();
+
+    /**
+     * Returns the created task count.
+     */
+    long getCreatedTaskCount();
+
+    /**
+     * Returns the incomplete task count.
+     */
+    long getIncompleteTaskCount();
+
+    /**
+     * Returns the pending task count.
+     */
+    long getPendingTaskCount();
+}
