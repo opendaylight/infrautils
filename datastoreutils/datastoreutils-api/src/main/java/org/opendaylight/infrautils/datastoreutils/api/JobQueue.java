1a16ec7eec44369b831b3fe83f42b975e1515aa8
Yakir Dorani <yakir.dorani@hpe.com>
2017-02-05 13:37:48 +0200

    Move DataStoreJobCoordinator from genius to infrautils
    Change-Id: Iacbf938b9bede086d754e5494d5b50fdbe28a705
    Signed-off-by: Yakir Dorani <yakir.dorani@hpe.com>

---
+++ datastoreutils/datastoreutils-api/src/main/java/org/opendaylight/infrautils/datastoreutils/api/JobQueue.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.infrautils.datastoreutils.api;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class JobQueue {
+    private final ConcurrentLinkedQueue<JobEntry> waitingEntries = new ConcurrentLinkedQueue<>();
+    private volatile JobEntry executingEntry;
+
+    public void addEntry(JobEntry entry) {
+        waitingEntries.add(entry);
+    }
+
+    public ConcurrentLinkedQueue<JobEntry> getWaitingEntries() {
+        return waitingEntries;
+    }
+
+    public JobEntry getExecutingEntry() {
+        return executingEntry;
+    }
+
+    public void setExecutingEntry(JobEntry executingEntry) {
+        this.executingEntry = executingEntry;
+    }
+}
