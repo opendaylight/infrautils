57fa9ccc959ea9f18cddf938c7fb7cda125bb5a3
Yakir Dorani <yakir.dorani@hpe.com>
2017-02-05 13:37:48 +0200

    Move DataStoreJobCoordinator from genius to infrautils
    This patch is almost an exact copy of DSJC from genius. The only different is
    the removal of an unnecessary map which contained a map of jobqueues.
    
    Change-Id: Iacbf938b9bede086d754e5494d5b50fdbe28a705
    Signed-off-by: Yakir Dorani <yakir.dorani@hpe.com>

---
+++ datastoreutils/datastoreutils-api/src/main/java/org/opendaylight/infrautils/datastoreutils/api/IDataStoreJobCoordinator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Hewlett-Packard Enterprise and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.datastoreutils.api;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+public interface IDataStoreJobCoordinator {
+
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker);
+
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, RollbackCallable rollbackWorker);
+
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, int maxRetries);
+
+    void enqueueJob(AbstractDataStoreJob job) throws InvalidJobException;
+
+    /**
+     * This is used by the external applications to enqueue a Job with an
+     * appropriate key. A JobEntry is created and queued appropriately.
+     */
+    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, RollbackCallable rollbackWorker,
+            int maxRetries);
+
+    long getIncompleteTaskCount();
+
+}
