260a7da77ec8736ee26865ecc033bfec7147c9c7
Michael Vorburger <vorburger@redhat.com>
2017-03-16 15:09:43 +0100

    CompareUtil JavaDoc incl. @deprecated safeCompare
    Change-Id: I5e4fcc66de0473f21cf59b0daaa2da3f8ea1bcdc
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- common/util/src/main/java/org/opendaylight/infrautils/utils/CompareUtil.java
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/CompareUtil.java
@@ -7,34 +7,52 @@
  */
 package org.opendaylight.infrautils.utils;
 
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
 import java.util.Collection;
+import java.util.Objects;
 
 public class CompareUtil {
+
+    /**
+     * Null safe equals.
+     * NB: This method is misnamed, it's a safe equals, not compare.
+     *
+     * @deprecated Prefer using the {@link Objects#equals(Object, Object)} (or
+     *             {@link Objects#deepEquals(Object, Object) for Arrays})
+     */
+    @Deprecated
     public static boolean safeCompare(Object obj1, Object obj2) {
         if (obj1 == obj2) {
             return true;
         }
         if (obj1 == null) {
-            return (obj2 == null);
+            return obj2 == null;
         }
         return obj1.equals(obj2);
     }
 
+    /**
+     * Null safe compareTo.
+     * Consider using JDK's
+     * {@link Objects#compare(Object, Object, java.util.Comparator)} or Guava's
+     * {@link ComparisonChain} and {@link Ordering} instead of this.
+     */
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static int safeCompareTo(Comparable obj1, Comparable obj2) {
         if (obj1 == obj2) {
             return 0;
         }
-        if ((obj1 == null) && (obj2 != null)) {
+        if (obj1 == null && obj2 != null) {
             return 1;
-        } else if ((obj1 != null) && (obj2 == null)) {
+        } else if (obj1 != null && obj2 == null) {
             return -1;
         }
         return obj1.compareTo(obj2);
     }
 
-    public static <T> void calculateDiff(Collection<T> newCollection, Collection<T> oldCollection, Collection<T> added, Collection<T> removed,
-            Collection<T> retain) {
+    public static <T> void calculateDiff(Collection<T> newCollection, Collection<T> oldCollection, Collection<T> added,
+            Collection<T> removed, Collection<T> retain) {
         retain.addAll(newCollection);
         added.addAll(newCollection);
         removed.addAll(oldCollection);
