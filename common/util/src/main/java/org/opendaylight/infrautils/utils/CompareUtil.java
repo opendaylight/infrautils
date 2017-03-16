ce4af6511a31a010990b1a3688ec452b70c77534
Michael Vorburger <vorburger@redhat.com>
2017-03-16 15:09:43 +0100

    CompareUtil JavaDoc incl. @deprecated safeCompare
    Change-Id: I5e4fcc66de0473f21cf59b0daaa2da3f8ea1bcdc
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- common/util/src/main/java/org/opendaylight/infrautils/utils/CompareUtil.java
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/CompareUtil.java
@@ -7,9 +7,21 @@
  */
 package org.opendaylight.infrautils.utils;
 
+import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
 import java.util.Collection;
+import java.util.Objects;
 
 public class CompareUtil {
+
+    /**
+     * Null safe equals.
+     * NB: This method is misnamed, it's a safe equals, not compare.
+     *
+     * @deprecated Prefer using the {@link Objects#equals(Object, Object)} (or
+     *             {@link Objects#deepEquals(Object, Object) for Arrays})
+     */
+    @Deprecated
     public static boolean safeCompare(Object obj1, Object obj2) {
         if (obj1 == obj2) {
             return true;
@@ -20,6 +32,12 @@ public class CompareUtil {
         return obj1.equals(obj2);
     }
 
+    /**
+     * Null safe compareTo.
+     * Consider using JDK's
+     * {@link Objects#compare(Object, Object, java.util.Comparator)} or Guava's
+     * {@link ComparisonChain} and {@link Ordering} instead of this.
+     */
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static int safeCompareTo(Comparable obj1, Comparable obj2) {
         if (obj1 == obj2) {
