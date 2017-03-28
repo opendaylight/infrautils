771c80d67fa52d651cc16d80a86e1eee0780f347
Robert Varga <robert.varga@pantheon.tech>
2017-03-28 02:24:40 +0200

    Use Strings.isNullOrEmpty()
    Guava gives us the basic string check, using it makes emptyString()
    much more trivial.
    
    Change-Id: I9792eb18ffc48ca854c95b0b3eb541f79b3e8275
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/util/src/main/java/org/opendaylight/infrautils/utils/StringUtil.java
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/StringUtil.java
@@ -9,6 +9,7 @@ package org.opendaylight.infrautils.utils;
 
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
 import java.lang.reflect.Method;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -249,12 +250,8 @@ public class StringUtil {
         return (num != NO_VALUE);
     }
 
-    public static boolean emptyString(String str) {
-        if ((str == null) || (str.length() == 0)) {
-            return true;
-        }
-        str = str.trim();
-        return (str.length() == 0);
+    public static boolean emptyString(final String str) {
+        return Strings.isNullOrEmpty(str) || str.trim().isEmpty();
     }
 
     public static String fullMethodSignatureAsString(final Object target, final Method method, final Object[] args) {
