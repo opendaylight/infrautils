ffcfc8d94fbeb265eba9e19c767e2148fbb1bc3b
Michael Vorburger <vorburger@redhat.com>
2017-04-06 12:15:40 +0200

    JobCoordinatorMonitor interface factored out of main JobCoordinator
    It's cleaner to have a simple JobCoordinator interface which is not
    encumbered by monitoring methods which only very few consumers need to
    see (NB those methods were introduced just for tests).
    
    Change-Id: I199729b5c60f0450e8be2b1985f8acd6dc589dd6
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ jobcoordinator/jobcoordinator-api/src/main/java/org/opendaylight/infrautils/jobcoordinator/JobCoordinatorMonitor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.jobcoordinator;
+
+/**
+ * Monitor for {@link JobCoordinator} metrics.
+ */
+public interface JobCoordinatorMonitor {
+
+    /**
+     * Returns the cleared task count.
+     */
+    long getClearedTaskCount();
+
+    /**
+     * Returns the created task count.
+     */
+    long getCreatedTaskCount();
+
+    /**
+     * Returns the incomplete task count.
+     */
+    long getIncompleteTaskCount();
+
+    /**
+     * Returns the pending task count.
+     */
+    long getPendingTaskCount();
+
+}
