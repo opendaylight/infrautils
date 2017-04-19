41c22e26f78e7a528eeed6666714c9561a2153c1
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2017-04-19 19:00:37 +0200

    Executors factory and utility methods
    Kickstarted as a helper to conveniently specify a name prefix and
    a logger for the executor threads.
    
    Change-Id: I0e1bd0e20ca006e0b18e2aaa6e7d0f35fdca5415
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

---
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/concurrent/Executors.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.infrautils.utils.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import org.slf4j.Logger;
+
+/**
+ * Additional factory and utility methods for executors.
+ */
+public final class Executors {
+
+    private Executors() {
+    }
+
+    /**
+     * Creates a single thread executor with a {@link ThreadFactory} that uses
+     * the provided prefix for its thread names and logs uncaught exceptions
+     * with the specified {@link Logger}.
+     *
+     * @param namePrefix Prefix for this executor thread names
+     * @param logger Logger used to log uncaught exceptions
+     * @return the newly created single-threaded Executor
+     */
+    public static ExecutorService newSingleThreadExecutor(final String namePrefix, final Logger logger) {
+        return java.util.concurrent.Executors.newSingleThreadExecutor(
+                ThreadFactoryProvider.builder()
+                        .namePrefix(namePrefix)
+                        .logger(logger)
+                        .build()
+                        .get());
+    }
+}
