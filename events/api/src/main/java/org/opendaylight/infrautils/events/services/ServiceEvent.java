e76e4e314dc1148ab5634dfced686870afacaaea
Michael Vorburger <vorburger@redhat.com>
2017-04-23 05:44:26 +0200

    Events API & Impl
    Change-Id: I5c30cb558836d01d6fecca02e08cab9fe60e96fe
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ events/api/src/main/java/org/opendaylight/infrautils/events/services/ServiceEvent.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.events.services;
+
+import java.util.Optional;
+import org.immutables.value.Value;
+
+/**
+ * Event related to a dynamic service, such as the OSGi service registry.
+ * In test environments, this may be emitted by a dependency injection framework such as e.g. Guice as well.
+ *
+ * @author Michael Vorburger.ch
+ */
+public interface ServiceEvent<T> {
+
+    Class<T> getServiceType();
+
+    @Value.Parameter(false)
+    Optional<String> filter();
+
+}
