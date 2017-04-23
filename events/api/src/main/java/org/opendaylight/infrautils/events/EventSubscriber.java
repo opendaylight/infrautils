8140b821383269b3f3ade802c63b3a6227b8ce0d
Michael Vorburger <vorburger@redhat.com>
2017-04-23 05:44:26 +0200

    Events API
    Change-Id: I5c30cb558836d01d6fecca02e08cab9fe60e96fe
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ events/api/src/main/java/org/opendaylight/infrautils/events/EventSubscriber.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.events;
+
+/**
+ * Subscribes to (AKA "registers for") event messages.
+ *
+ * @author Michael Vorburger.ch
+ */
+public interface EventSubscriber {
+
+    /**
+     * Subscribe.
+     * @param <E> Even type (note that there is no Event base type / marker interface)
+     * @param eventClass Event type
+     * @param listener Event listener
+     * @return Subscription used (only) to {@link #unsubscribe(Subscription)}
+     */
+    <E> Subscription subscribe(Class<E> eventClass, EventListener<? super E> listener);
+
+    void unsubscribe(Subscription subscription) throws IllegalArgumentException;
+
+    interface Subscription { }
+}
