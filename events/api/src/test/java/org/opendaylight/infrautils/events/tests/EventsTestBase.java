0d20e3cea9b3a3a47a80e5468669d94485d7cec2
Michael Vorburger <vorburger@redhat.com>
2017-04-23 05:44:26 +0200

    Events API
    Change-Id: I5c30cb558836d01d6fecca02e08cab9fe60e96fe
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ events/api/src/test/java/org/opendaylight/infrautils/events/tests/EventsTestBase.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.events.tests;
+
+import org.junit.After;
+import org.mockito.Mock;
+import org.opendaylight.infrautils.events.EventBus;
+
+/**
+ * Some concepts shared among tests here.
+ * @author Michael Vorburger.ch
+ */
+public abstract class EventsTestBase {
+
+    protected abstract static class ParentEvent { }
+
+    protected static class ChildEvent extends ParentEvent { }
+
+    protected interface AnotherEvent { }
+
+    @Mock EventBus bus;
+
+    // EventBus is asynchronous, must use volatile:
+    volatile boolean childEventReceived = false;
+    volatile boolean parentEventReceived = false;
+    volatile boolean anotherEventReceived = false;
+
+    @After
+    public void tearDown() throws Exception {
+        bus.close();
+    }
+
+}
