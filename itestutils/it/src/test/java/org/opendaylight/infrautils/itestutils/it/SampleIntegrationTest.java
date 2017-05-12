e372e7296a8093ce0e08cd7430157695e21519f7
Michael Vorburger <vorburger@redhat.com>
2017-05-12 06:11:55 +0200

    Bug 8438: New integration test utilities (itestutils)
    see Bugzilla description for more background re. what this is all about
    
    Change-Id: I1d2524c874a371a7c7d63ebd8cbb9dfb0678bc44
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ itestutils/it/src/test/java/org/opendaylight/infrautils/itestutils/it/SampleIntegrationTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.itestutils.it;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.junit.Test;
+import org.opendaylight.infrautils.itestutils.AbstractIntegrationTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Example Integration Test.
+ * @author Michael Vorburger.ch
+ */
+// @ExamReactorStrategy(PerClass.class) IFF the default PerMethod (which provides better isolation!) really is too slow?
+@SuppressFBWarnings("NP_NONNULL_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
+public class SampleIntegrationTest extends AbstractIntegrationTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SampleIntegrationTest.class);
+
+    @Test
+    public void testEmptyJustToMakeSureKarafStartedOK() {
+        LOG.info("info log is not visible, as not enabled");
+        LOG.warn("warn log is visible");
+    }
+
+//    @Test
+//    public void testGoogleTruthWorksInsideOSGi() {
+//        assertThat(Boolean.TRUE).isTrue();
+//    }
+
+}
