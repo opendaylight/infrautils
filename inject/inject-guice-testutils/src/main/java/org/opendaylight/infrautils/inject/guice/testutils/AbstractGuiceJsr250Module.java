cfdd44dd84f72047c4f2ab6852a974d76249ffef
Michael Vorburger <vorburger@redhat.com>
2017-06-01 07:01:02 +0200

    Add bind2ToInstance to AbstractGuiceJsr250Module
    This is already used in ElanServiceTestModule, and also needed in
    FibManagerTestModule, so instead of copy/pasting it, being move up
    refactored here.
    
    Change-Id: I3b5e11236c1e756ba98fbe9ee10b1dcfe9dd6f98
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- inject/inject-guice-testutils/src/main/java/org/opendaylight/infrautils/inject/guice/testutils/AbstractGuiceJsr250Module.java
+++ inject/inject-guice-testutils/src/main/java/org/opendaylight/infrautils/inject/guice/testutils/AbstractGuiceJsr250Module.java
@@ -34,4 +34,21 @@ public abstract class AbstractGuiceJsr250Module extends AbstractModule {
 
     protected abstract void configureBindings() throws Exception;
 
+    /**
+     * Binds instance to both the interfaceClass as well as the implementationClass.
+     *
+     * @param interfaceClass class type of an interface
+     * @param implementationClass  class type of implementing class
+     * @param instance an instance implementing both interfaceClass & implementationClass
+     */
+    @SuppressWarnings("unchecked")
+    protected <T> void bind2ToInstance(Class<T> interfaceClass, Class<? extends T> implementationClass, T instance) {
+        if (implementationClass.equals(interfaceClass)) {
+            throw new IllegalArgumentException("interfaceClass should not be the same as implementationClass: "
+                    + interfaceClass + "; " + implementationClass);
+        }
+        bind(interfaceClass).toInstance(instance);
+        bind((Class<T>) implementationClass).toInstance(instance);
+    }
+
 }
