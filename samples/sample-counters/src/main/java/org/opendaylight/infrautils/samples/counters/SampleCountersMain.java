a30398f2f59a1c208f1269b73f73f95474d32b3b
David Suarez <david.suarez.fuentes@gmail.com>
2017-08-02 15:08:43 +0200

    Add checkstyle enforcement to this module
    Add checkstyle enforcement to this module fixing the issues.
    
    Change-Id: I2e3d6c7043ad7624065da7968bf20cb8b2050eef
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- samples/sample-counters/src/main/java/org/opendaylight/infrautils/samples/counters/SampleCountersMain.java
+++ samples/sample-counters/src/main/java/org/opendaylight/infrautils/samples/counters/SampleCountersMain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Hewlett Packard Enterprise, Co. and others. All rights reserved.
+ * Copyright (c) 2016, 2017 Hewlett Packard Enterprise, Co. and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,8 +9,11 @@
 package org.opendaylight.infrautils.samples.counters;
 
 import org.opendaylight.infrautils.counters.api.OccurenceCounter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SampleCountersMain {
+    private static final Logger LOG = LoggerFactory.getLogger(SampleCountersMain.class);
 
     public void init() {
         // Running all the example code in a different thread so the user will
@@ -23,33 +26,34 @@ public class SampleCountersMain {
 
         @Override
         public void run() {
-            System.out.println("\nStarted Counters Client Sample!");
-            System.out.println("Change the writelog configuration to true at: org.opendaylight.counters.cfg");
-            System.out.println("You will be able to see in the log, prints of the counters values.");
-            SampleCounters.initial_counter.inc();
+            LOG.info("Started Counters Client Sample!");
+            LOG.info("Change the writelog configuration to true at: org.opendaylight.counters.cfg");
+            LOG.info("You will be able to see in the log, prints of the counters values.");
+            SampleCounters.INITIAL_COUNTER.inc();
             for (int i = 0; i < 1000; ++i) {
                 for (int j = 0; j < 1000; ++j) {
-                    SampleCounters.high_frequency_event.inc();
+                    SampleCounters.HIGH_FREQUENCY_EVENT.inc();
                 }
                 try {
                     Thread.sleep(5000);
                 } catch (InterruptedException e) {
+                    LOG.warn("Interrupted", e);
                     return;
                 }
-                SampleCounters.low_frequency_event.inc();
+                SampleCounters.LOW_FREQUENCY_EVENT.inc();
             }
-            System.out.println("Finished Counters Client!");
+            LOG.info("Finished Counters Client!");
         }
     }
 
     enum SampleCounters {
-        initial_counter, //
-        low_frequency_event, //
-        high_frequency_event;
+        INITIAL_COUNTER,
+        LOW_FREQUENCY_EVENT,
+        HIGH_FREQUENCY_EVENT;
 
-        private OccurenceCounter counter;
+        private final OccurenceCounter counter;
 
-        private SampleCounters() {
+        SampleCounters() {
             counter = new OccurenceCounter(getClass().getEnclosingClass().getSimpleName(), name(), name());
         }
 
