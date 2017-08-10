d3a09e06b1c9ac1c8de7209b1d146194f0dbab5b
Michael Vorburger <vorburger@redhat.com>
2017-08-10 14:37:56 +0530

    New ready order FunctionalityReady with *Notifier, to help order startup
    This came out of some thought I've given
    https://git.opendaylight.org/gerrit/#/c/61323/
    
    This is just the API; if there are no strong objections to this
    approach, I'd like to get this merged in, and the propose the actual
    FunctionalityReadyNotifierImpl body in a new follow-up change.
    
    Change-Id: I943f963c835bbec7f113b56281d28d713156f514
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ ready/impl/src/main/java/org/opendaylight/infrautils/ready/internal/FunctionalityReadyNotifierImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.ready.internal;
+
+import org.opendaylight.infrautils.ready.order.FunctionalityReady;
+import org.opendaylight.infrautils.ready.order.FunctionalityReadyNotifier;
+
+/**
+ * Implementation of {@link FunctionalityReadyNotifier}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class FunctionalityReadyNotifierImpl implements FunctionalityReadyNotifier {
+
+    @Override
+    public void register(Class<? extends FunctionalityReady> markerInterface) {
+        // TODO, incl. TDD IT
+        //   * check argument that it's an interface and not a class
+        //   * check OSGi registry for previously registered duplicates (allow only 1)
+        //   * create new instance from interface not class
+        //   * OSGi registry register it
+        //   * INFO log it
+    }
+
+    @Override
+    public void unregister(Class<? extends FunctionalityReady> markerInterface) {
+        // TODO, incl. TDD IT
+    }
+
+}
