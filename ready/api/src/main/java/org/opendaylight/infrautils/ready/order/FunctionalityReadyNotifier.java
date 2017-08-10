f08aa56a1acc402734291ee000d4622af28fd5d5
Michael Vorburger <vorburger@redhat.com>
2017-08-10 14:37:56 +0530

    New ready order FunctionalityReady with *Notifier, to help order startup
    This came out of some thought I've given
    https://git.opendaylight.org/gerrit/#/c/61323/
    
    This is just the API; if there are no strong objections to this
    approach, I'd like to get this merged in, and the propose the actual
    FunctionalityReadyNotifierImpl body in a new follow-up change.
    
    Change-Id: I943f963c835bbec7f113b56281d28d713156f514
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ ready/api/src/main/java/org/opendaylight/infrautils/ready/order/FunctionalityReadyNotifier.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.ready.order;
+
+/**
+ * Service to register & unregister {@link FunctionalityReady} markers.
+ *
+ * @author Michael Vorburger.ch
+ */
+public interface FunctionalityReadyNotifier {
+
+    void register(Class<? extends FunctionalityReady> markerInterface);
+
+    void unregister(Class<? extends FunctionalityReady> markerInterface);
+
+}
