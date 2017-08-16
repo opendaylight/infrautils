4504092a2c3a352b601ac9e326c0665ef05636cd
Faseela K <faseela.k@ericsson.com>
2017-08-16 16:53:42 +0530

    Implementation for Push Model of Status Reporting
    This patch has the basic implementation on how to register a service
    for status reporting, and how to report the status.
    More detailed implementation of the shell command on how to get cluster
    wide service status will come in subsequent patches
    
    Change-Id: Iac4d0eea7eb0d6704fc77114053da2a2f82335e1
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/impl/src/test/java/org/opendaylight/infrautils/diagstatus/test/DiagStatusTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus.test;
+
+import javax.inject.Inject;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.opendaylight.infrautils.diagstatus.DiagStatusService;
+import org.opendaylight.infrautils.diagstatus.ServiceDescriptor;
+import org.opendaylight.infrautils.diagstatus.ServiceState;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.infrautils.testutils.LogRule;
+
+/**
+ * Component tests for diagstatus.
+ *
+ * @author Faseela K
+ */
+
+public class DiagStatusTest {
+
+    public @Rule LogRule logRule = new LogRule();
+    public @Rule MethodRule guice = new GuiceRule(new DiagStatusTestModule());
+
+    @Inject DiagStatusService diagStatusService;
+
+    @Test
+    public void testDiagStatus() {
+        String testService1 = "testService";
+        diagStatusService.register(testService1);
+        // Verify if "testService" got registered with STARTING state.
+        ServiceDescriptor serviceDescriptor = diagStatusService.getServiceDescriptor(testService1);
+        Assert.assertEquals(serviceDescriptor.getServiceState(), ServiceState.STARTING);
+
+        // Verify if "testService" status is updated as OPERATIONAL.
+        diagStatusService.report(testService1, ServiceState.OPERATIONAL, "service is UP");
+        serviceDescriptor = diagStatusService.getServiceDescriptor(testService1);
+        Assert.assertEquals(serviceDescriptor.getServiceState(), ServiceState.OPERATIONAL);
+
+        // TODO add JXM based Junits to see if the service state is getting retrieved properly.
+
+    }
+}
