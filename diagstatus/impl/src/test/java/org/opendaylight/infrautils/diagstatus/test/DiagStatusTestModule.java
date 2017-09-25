49feb5516a3e6c636d1fec343e3796004c7ef893
Michael Vorburger <vorburger@redhat.com>
2017-09-25 23:48:54 +0200

    First integration of infratutils' ready with diagstatus
    including a few minor clean-ups (not related to "ready" integration)
    
    More to come - let's go step by step.
    
    Change-Id: If3afecd1e36a4daf6844fdb682bbff95ba864579
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- diagstatus/impl/src/test/java/org/opendaylight/infrautils/diagstatus/test/DiagStatusTestModule.java
+++ diagstatus/impl/src/test/java/org/opendaylight/infrautils/diagstatus/test/DiagStatusTestModule.java
@@ -14,20 +14,34 @@ import java.util.List;
 import org.opendaylight.infrautils.diagstatus.DiagStatusService;
 import org.opendaylight.infrautils.diagstatus.ServiceStatusProvider;
 import org.opendaylight.infrautils.diagstatus.internal.DiagStatusServiceImpl;
-import org.opendaylight.infrautils.diagstatus.internal.DiagStatusServiceImplMBean;
 import org.opendaylight.infrautils.inject.guice.testutils.AbstractGuiceJsr250Module;
+import org.opendaylight.infrautils.ready.SystemReadyListener;
+import org.opendaylight.infrautils.ready.SystemReadyMonitor;
+import org.opendaylight.infrautils.ready.SystemState;
+import org.ops4j.pax.cdi.api.OsgiService;
 
 /**
  * Dependency Injection Wiring for {@link DiagStatusTest}.
  *
- * @author Faseela K
+ * @author Faseela K & Michael Vorburger.ch
  */
 public class DiagStatusTestModule extends AbstractGuiceJsr250Module {
 
     @Override
     protected void configureBindings() throws UnknownHostException {
         bind(DiagStatusService.class).to(DiagStatusServiceImpl.class);
-        bind(DiagStatusServiceImplMBean.class).to(DiagStatusServiceImpl.class);
         bind(new TypeLiteral<List<ServiceStatusProvider>>() {}).toInstance(Collections.emptyList());
+        bind(SystemReadyMonitor.class).annotatedWith(OsgiService.class).toInstance(new SystemReadyMonitor() {
+
+            @Override
+            public void registerListener(SystemReadyListener listener) {
+                // NOOP
+            }
+
+            @Override
+            public SystemState getSystemState() {
+                return SystemState.ACTIVE;
+            }
+        });
     }
 }
