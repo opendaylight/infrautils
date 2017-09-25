b2c3850c30fce54c4cc0118865be1deca0564dda
Faseela K <faseela.k@ericsson.com>
2017-09-26 00:10:10 +0530

    DiagStatus Integration Tests
    Change-Id: I89d61a66a74b49c6ec11eab0ca23e10a77b404a2
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/it/src/test/java/org/opendaylight/infrautils/diagstatus/it/DiagStatusIT.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus.it;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+
+import javax.inject.Inject;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.infrautils.diagstatus.DiagStatusService;
+import org.opendaylight.infrautils.diagstatus.ServiceDescriptor;
+import org.opendaylight.infrautils.diagstatus.ServiceState;
+import org.opendaylight.infrautils.itestutils.AbstractIntegrationTest;
+import org.ops4j.pax.exam.options.UrlReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * DiagStatus Integration Test.
+ * @author Faseela K
+ */
+public class DiagStatusIT extends AbstractIntegrationTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DiagStatusIT.class);
+
+    @Inject
+    DiagStatusService diagStatusService;
+
+    @Override
+    protected UrlReference featureRepositoryURL() {
+        return maven()
+                .groupId("org.opendaylight.infrautils")
+                .artifactId("infrautils-features")
+                .classifier("features")
+                .type("xml")
+                .versionAsInProject();
+    }
+
+    @Override
+    protected String featureName() {
+        return "odl-infrautils-diagstatus";
+    }
+
+    @Test
+    public void testDiagStatusFeatureLoad() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testDiagStatusPushModel() {
+        String testService1 = "testService";
+        diagStatusService.register(testService1);
+        // Verify if "testService" got registered with STARTING state.
+        ServiceDescriptor serviceDescriptor = diagStatusService.getServiceDescriptor(testService1);
+        Assert.assertEquals(serviceDescriptor.getServiceState(), ServiceState.STARTING);
+
+        // Verify if "testService" status is updated as OPERATIONAL.
+        diagStatusService.report(testService1, ServiceState.OPERATIONAL, "service is UP");
+        serviceDescriptor = diagStatusService.getServiceDescriptor(testService1);
+        Assert.assertEquals(serviceDescriptor.getServiceState(), ServiceState.OPERATIONAL);
+    }
+
+}
