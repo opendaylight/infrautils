e64850840e15d29c820373202582c23c0b7f9a6f
Faseela K <faseela.k@ericsson.com>
2017-09-25 23:08:12 +0530

    Pull Model For DiagStatus
    Change-Id: I22c1b09f11de41bb10aa3bc91ec778f0ebdce69f
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ diagstatus/api/src/main/java/org/opendaylight/infrautils/diagstatus/ServiceStatusProvider.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus;
+
+/**
+ * Apps can register an implementation of this interface in the OSGi service registry
+ * if they want to expose their live status.
+ *
+ * @author Faseela K
+ */
+public interface ServiceStatusProvider {
+
+    /**
+     * Called back whenever a query comes for the current status of
+     * the registered services.
+     */
+    ServiceDescriptor getServiceDescriptor();
+}
