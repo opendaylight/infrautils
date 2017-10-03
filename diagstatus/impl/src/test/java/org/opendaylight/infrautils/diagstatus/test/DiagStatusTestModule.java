40b1a1c48edfeed04c87354231b6da3c62ce2f0b
Faseela K <faseela.k@ericsson.com>
2017-10-03 10:25:37 +0530

    NotCompliantMBeanException after the recent MBean refactoring
    Diagstatus is broken after the refactoring refactoring of MBean Impl
    The NotCompliantMBeanException is caused, among other things, by failing to follow this convention
    'ConcreteClassName' implements 'ConcreteClassNameMBean'
    
    Change-Id: I0f99a5c95845e5b25b8a9a75f9498b1e5520ced2
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- diagstatus/impl/src/test/java/org/opendaylight/infrautils/diagstatus/test/DiagStatusTestModule.java
+++ diagstatus/impl/src/test/java/org/opendaylight/infrautils/diagstatus/test/DiagStatusTestModule.java
@@ -13,9 +13,9 @@ import java.util.Collections;
 import java.util.List;
 import org.opendaylight.infrautils.diagstatus.DiagStatusService;
 import org.opendaylight.infrautils.diagstatus.ServiceStatusProvider;
+import org.opendaylight.infrautils.diagstatus.internal.DiagStatusImpl;
+import org.opendaylight.infrautils.diagstatus.internal.DiagStatusImplMBean;
 import org.opendaylight.infrautils.diagstatus.internal.DiagStatusServiceImpl;
-import org.opendaylight.infrautils.diagstatus.internal.DiagStatusServiceMBean;
-import org.opendaylight.infrautils.diagstatus.internal.DiagStatusServiceMBeanImpl;
 import org.opendaylight.infrautils.inject.guice.testutils.AbstractGuiceJsr250Module;
 
 /**
@@ -28,7 +28,7 @@ public class DiagStatusTestModule extends AbstractGuiceJsr250Module {
     @Override
     protected void configureBindings() throws UnknownHostException {
         bind(DiagStatusService.class).to(DiagStatusServiceImpl.class);
-        bind(DiagStatusServiceMBean.class).to(DiagStatusServiceMBeanImpl.class);
+        bind(DiagStatusImplMBean.class).to(DiagStatusImpl.class);
         bind(new TypeLiteral<List<ServiceStatusProvider>>() {}).toInstance(Collections.emptyList());
     }
 }
