ab973b3a7210fd8648b248c67018c42b0fd7ab3d
Stephen Kitt <skitt@redhat.com>
2017-10-05 13:20:51 +0200

    RollbackCallable: rework as consumer
    The callable’s job here is to act on a single failed job’s results, so
    instead of modelling it as a data object, model it as a function
    consuming the failed job’s futures and producing its own.
    
    We could just replace RollbackCallable with
    Function<List<ListenableFuture<Void>>, List<ListenableFuture<Void>>
    but I think keeping the class has its merits, if only for
    documentation and readability.
    
    Change-Id: I049f15096afe1c24ecad4e2282f94830fc471398
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- jobcoordinator/jobcoordinator-api/src/main/java/org/opendaylight/infrautils/jobcoordinator/RollbackCallable.java
+++ jobcoordinator/jobcoordinator-api/src/main/java/org/opendaylight/infrautils/jobcoordinator/RollbackCallable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright © 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,26 +9,20 @@ package org.opendaylight.infrautils.jobcoordinator;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.List;
-import java.util.concurrent.Callable;
-import org.eclipse.jdt.annotation.Nullable;
+import java.util.function.Function;
 
 /**
- * A callable which runs in case a job task fails. It holds the futures that
- * were returned by the last failing job.
+ * A callable which runs in case a job task fails. It consumes the futures that were returned by the last failing
+ * job, and returns the futures corresponding to the corrective transactions.
  */
-public abstract class RollbackCallable implements Callable<List<ListenableFuture<Void>>> {
-
-    private volatile @Nullable List<ListenableFuture<Void>> futures;
-
-    public @Nullable List<ListenableFuture<Void>> getFutures() {
-        return futures;
-    }
-
+public interface RollbackCallable extends Function<List<ListenableFuture<Void>>, List<ListenableFuture<Void>>> {
     /**
-     * Sets the failed futures which resulted in this RollbackCallable being
-     * called.
+     * Roll back the transaction which led to the provided failed futures (futures resulting from the failed operation
+     * &mdash; the futures themselves aren't necessarily failed).
+     *
+     * @param failedFutures The futures from the failed job.
+     * @return The corrective roll back's resulting futures.
      */
-    public void setFutures(@Nullable List<ListenableFuture<Void>> futures) {
-        this.futures = futures;
-    }
+    @Override
+    List<ListenableFuture<Void>> apply(List<ListenableFuture<Void>> failedFutures);
 }
