45372b3fb6a32b971aab4dc4950b218ac3b0f90d
Michael Vorburger <vorburger@redhat.com>
2017-10-18 12:41:36 +0200

    StackTraces getCallerMethodName()
    I'd like to use this in org.opendaylight.genius.infra.FutureRpcResults
    to offer a variant without the explicit String rpcMethodName, and
    instead implicitly get it from this.
    
    This is motivated by seeing first users starting to adopt
    FutureRpcResults, e.g. in I04553678580ff53217c7757594cdd836aa8abfae,
    getting that rpcMethodName wrong (not once but twice).
    
    Change-Id: Id67f619fabd390485b44420c5d5669f12e5fd82d
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/StackTraces.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.utils;
+
+/**
+ * Utilities for {@link StackTraceElement} & Co.
+ *
+ * @author Michael Vorburger.ch, based on an idea by Stephen Kitt
+ */
+public class StackTraces {
+
+    public static String getCallerMethodName() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        if (stackTrace.length > 2) {
+            StackTraceElement element = stackTrace[2];
+            return element.getMethodName();
+        } else {
+            // This normally should never happen...
+            throw new IllegalStateException("Stack Trace is less than 2 levels deep?!");
+        }
+    }
+
+    // TODO add (sth like) org.opendaylight.controller.md.sal.trace.dom.impl.TracingBroker's printStackTraceElements()
+
+    // TODO add a suitable abstraction here which will allow impl to be switched to Java 9's JEP 259: Stack-Walking API
+    // by looking at e.g. what org.opendaylight.controller.md.sal.trace.closetracker.impl.CloseTrackedTrait does
+
+}
