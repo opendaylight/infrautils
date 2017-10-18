ed43ec83bd77bbee2e3df78bb843092876098606
Tom Pantelis <tompantelis@gmail.com>
2017-10-18 13:25:35 -0400

    Add JobCoordinator test utils (from genius)
    Since the JobCoordinator now resides here and the one in
    genius is deprecated, it seems to make sense to move the
    associated test utilities here as well, specifically
    JobCoordinatorEventsWaiter and TestableJobCoordinatorEventsWaiter.
    Also AsyncEventsWaiter since they use that interface. The ones
    in genius will be deprecated.
    
    The other option is to leave them in genius and modify
    TestableJobCoordinatorEventsWaiter to use JobCoordinator or
    create a new version that does.
    
    Change-Id: I9ccd1660c90e266f95fa5711fdfafed429fc318a
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ testutils/src/main/java/org/opendaylight/infrautils/testutils/concurrent/AsyncEventsWaiter.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.testutils.concurrent;
+
+import org.awaitility.core.ConditionTimeoutException;
+
+/**
+ * Allows tests to wait for asynchronous event processing to be done.
+ *
+ * @author Michael Vorburger.ch
+ */
+public interface AsyncEventsWaiter extends AutoCloseable {
+
+    /**
+     * Wait by blocking calling thread until pending events have been processed
+     * by other threads in the background.  Implementations must have some
+     * sensible fixed timeout value.  This method is normally called from
+     * the JUnit Test main thread.
+     *
+     * @return true if if anything was pending to be processed and has been
+     *         processed, false if nothing needed to be
+     *
+     * @throws ConditionTimeoutException if timed out while waiting
+     */
+    boolean awaitEventsConsumption() throws ConditionTimeoutException;
+
+}
---
+++ testutils/src/main/java/org/opendaylight/infrautils/testutils/concurrent/TestableJobCoordinatorEventsWaiter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd., Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.testutils.concurrent;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.Matchers.is;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ConditionTimeoutException;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinatorMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class TestableJobCoordinatorEventsWaiter implements JobCoordinatorEventsWaiter {
+    private static final Logger LOG = LoggerFactory.getLogger(TestableJobCoordinatorEventsWaiter.class);
+
+    private final JobCoordinatorMonitor coordinatorMonitor;
+
+    @Inject
+    public TestableJobCoordinatorEventsWaiter(JobCoordinatorMonitor coordinatorMonitor) {
+        this.coordinatorMonitor = coordinatorMonitor;
+    }
+
+    @Override
+    public boolean awaitEventsConsumption() throws ConditionTimeoutException {
+        Awaitility.await("TestableJobCoordinatorEventsWaiter")
+            .atMost(30, SECONDS)
+            .pollDelay(0, MILLISECONDS)
+            .conditionEvaluationListener(condition -> LOG.info(
+                    "awaitEventsConsumption: Elapsed time {}s, remaining time {}s; incompleteTaskCount: {}",
+                    condition.getElapsedTimeInMS() / 1000, condition.getRemainingTimeInMS() / 1000,
+                    condition.getValue()))
+            .until(() -> coordinatorMonitor.getIncompleteTaskCount(), is(0L));
+        return true;
+    }
+
+    @Override
+    public void close() {
+    }
+}
