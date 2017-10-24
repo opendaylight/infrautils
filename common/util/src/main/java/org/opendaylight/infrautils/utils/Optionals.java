7d84888b39662e37db6f99e0c2d11a284bdb01ed
Michael Vorburger <vorburger@redhat.com>
2017-10-24 20:39:02 +0200

    Optionals ifPresent with CheckedConsumer
    Change-Id: I483ccb33e09aeea01489ae426587b31e3fecf922
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/Optionals.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.utils;
+
+import java.util.Optional;
+import org.opendaylight.infrautils.utils.function.CheckedConsumer;
+
+/**
+ * Utilities for Optionals.
+ *
+ * @author Michael Vorburger.ch
+ */
+public final class Optionals {
+
+    private Optionals() {
+    }
+
+    /**
+     * Like {@link Optional#ifPresent(java.util.function.Consumer)} but allows checked exceptions to be thrown.
+     */
+    public static <T, E extends Exception> void ifPresent(Optional<T> optional, CheckedConsumer<? super T, E> consumer)
+            throws E {
+        if (optional.isPresent()) {
+            consumer.accept(optional.get());
+        }
+    }
+}
