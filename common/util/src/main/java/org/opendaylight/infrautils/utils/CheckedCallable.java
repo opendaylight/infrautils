53c29317015e05a2b299c33f19d679609b8f33ca
Michael Vorburger <vorburger@redhat.com>
2017-10-26 19:39:51 +0200

    CheckedCallable
    Change-Id: I901a202b69ec86f3406beba312a955b0d55ed67f
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/CheckedCallable.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.utils;
+
+import java.util.concurrent.Callable;
+
+/**
+ * {@link Callable} which throws a specific generically parameterized type of
+ * checked exception instead of a fixed {@link Exception}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public interface CheckedCallable<V, E extends Exception> extends Callable<V> {
+
+    @Override
+    V call() throws E;
+
+}
