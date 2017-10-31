5ab6c960550432b0ffc13f3ca9d0ee419363ebaa
Tom Pantelis <tompantelis@gmail.com>
2017-10-31 17:25:09 -0400

    Add KeyedLocks
    Simply manages multiple re-rentrant locks by user key. This will be used to
    replace the static locks Map and code in https://github.com/opendaylight/netvirt/blob/master/vpnservice/neutronvpn/neutronvpn-api/src/main/java/org/opendaylight/netvirt/neutronvpn/api/utils/NeutronUtils.java.
    
    Change-Id: I817d8a7438f5ab750c35d502742a26fb3a710f23
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/concurrent/KeyedLocks.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2017 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.utils.concurrent;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.GuardedBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Manages multiple ReentrantLocks by key.
+ *
+ * @param <T> the key type
+ * @author Thomas Pantelis
+ */
+public class KeyedLocks<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(KeyedLocks.class);
+
+    private static class CountingReentrantLock extends ReentrantLock {
+        private static final long serialVersionUID = 1;
+
+        AtomicInteger useCount = new AtomicInteger(1);
+    }
+
+    @GuardedBy("locks")
+    private final Map<T, CountingReentrantLock> locks = new HashMap<>();
+
+    /**
+     * Tries to acquire the lock for the given key if it is not held by another thread within the given waiting time.
+     * See {@link ReentrantLock#tryLock(long, TimeUnit)} for more details.
+     *
+     * @param lockKey the key to lock
+     * @param timeout the time to wait for the lock
+     * @param unit the time unit of the timeout argument
+     * @return {@code true} if the lock was free and was acquired by the current thread, or the lock was already held
+     *         by the current thread, otherwise {@code false} if the waiting time elapsed before the lock could be
+     *         acquired or the current thread was interrupted.
+     */
+    public boolean tryLock(@Nonnull T lockKey, long timeout, TimeUnit unit) {
+        LOG.debug("tryLock {}, time {}, unit: {}", lockKey, timeout, unit);
+        return doLock(lockKey, lock -> {
+            try {
+                final boolean locked = lock.tryLock(timeout, unit);
+                if (!locked) {
+                    lock.useCount.decrementAndGet();
+                    LOG.debug("tryLock {} - already locked - count: {}", lockKey, lock.useCount);
+                }
+
+                return locked;
+            } catch (InterruptedException e) {
+                lock.useCount.decrementAndGet();
+                LOG.warn("tryLock was interrrupted for key {} - remaining use count: {}", lockKey, lock.useCount);
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Acquires the lock for the given key only if it is not held by another thread at the time of invocation.
+     * See {@link ReentrantLock#tryLock()} for more details.
+     *
+     * @param lockKey the key to lock
+     * @return {@code true} if the lock was free and was acquired by the current thread, or the lock was already held
+     *         by the current thread, otherwise {@code false} otherwise
+     */
+    public boolean tryLock(@Nonnull T lockKey) {
+        LOG.debug("tryLock {}", lockKey);
+        return doLock(lockKey, lock -> {
+            final boolean locked = lock.tryLock();
+            if (!locked) {
+                lock.useCount.decrementAndGet();
+                LOG.debug("tryLock {} - already locked - count: {}", lockKey, lock.useCount);
+            }
+
+            return locked;
+        });
+    }
+
+    /**
+     * Acquires the lock for the given key.
+     * See {@link ReentrantLock#lock()} for more details.
+     *
+     * @param lockKey the key to lock
+     */
+    public void lock(@Nonnull T lockKey) {
+        LOG.debug("lock {}", lockKey);
+        doLock(lockKey, lock -> {
+            lock.lock();
+            return true;
+        });
+    }
+
+    private boolean doLock(T lockKey, Function<CountingReentrantLock, Boolean> lockFunction) {
+        CountingReentrantLock lock;
+        synchronized (locks) {
+            lock = locks.get(lockKey);
+            if (lock == null) {
+                LOG.debug("Creating new Lock for {}", lockKey);
+                CountingReentrantLock newLock = new CountingReentrantLock();
+                locks.put(lockKey, newLock);
+                return lockFunction.apply(newLock);
+            } else {
+                lock.useCount.incrementAndGet();
+                LOG.debug("Lock for {} aleady exists - new count {}", lockKey, lock.useCount);
+            }
+        }
+
+        return lockFunction.apply(lock);
+    }
+
+    /**
+     * Attempts to release the lock for the given key.
+     * See {@link ReentrantLock#unlock()} for more details.
+     *
+     * @param lockKey the key to unlock
+     * @throws IllegalMonitorStateException if the current thread does not hold this lock
+     */
+    public void unlock(@Nonnull T lockKey) {
+        LOG.debug("unlock {}", lockKey);
+        final CountingReentrantLock lock;
+        synchronized (locks) {
+            lock = locks.get(lockKey);
+            if (lock != null) {
+                LOG.debug("Found Lock for {} - count: {}", lockKey, lock.useCount);
+                if (lock.useCount.decrementAndGet() <= 0) {
+                    LOG.debug("Rmoving Lock for {}", lockKey);
+                    locks.remove(lockKey);
+                }
+            } else {
+                throw new IllegalMonitorStateException("Lock for key " + lockKey
+                        + " is not owned by the current thread");
+            }
+        }
+
+        lock.unlock();
+    }
+
+    @VisibleForTesting
+    int size() {
+        synchronized (locks) {
+            return locks.size();
+        }
+    }
+}
