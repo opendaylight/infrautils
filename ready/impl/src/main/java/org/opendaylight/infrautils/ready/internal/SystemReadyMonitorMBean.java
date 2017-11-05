31e9b87576d488dfbbb14f25eae8cb3a1ff0ca94
Faseela K <faseela.k@ericsson.com>
2017-11-06 00:46:24 +0530

    Expose system ready state as MBean
    This will help applications to acquire cluster wide system status
    if required.
    
    Change-Id: I4cf048a0927e120a699ad88f7736e78906a59685
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ ready/impl/src/main/java/org/opendaylight/infrautils/ready/internal/SystemReadyMonitorMBean.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.infrautils.ready.internal;
+
+/**
+ * Service which exposes system ready-ness state as MBean.
+ *
+ * @author Faseela K
+ */
+public interface SystemReadyMonitorMBean {
+    /**
+     * Returns the overall state of the system based on the status of bundle bringup for the features.
+     *
+     * @return SystemState Current state of the system
+     */
+    String getCurrentSystemState();
+}
