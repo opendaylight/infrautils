15b49c421c9c988d2f7e478cba4fe98429a5826a
Michael Vorburger <vorburger@redhat.com>
2017-11-09 02:06:03 +0100

    Metrics Step 1: Basic infrautils.metrics API & Impl bundles outline
    Not yet usable as is, but for code review purposes intentionally going
    to build this up step by step for folks to follow along more easily.
    
    Next step after this: jobcoordinator-impl dependency to metrics-api,
    first usage of metrics for JobCoordinatorMonitor, and correct OSGi
    service registration, Karaf features etc. stuff.
    
    Background in https://jira.opendaylight.org/browse/INFRAUTILS-19
    
    Change-Id: Ia11f9973a018cfa392a35358a4467035ecf84e93
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ metrics/impl/src/test/java/org/opendaylight/infrautils/metrics/tests/MetricProviderTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.metrics.tests;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.codahale.metrics.Meter;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.opendaylight.infrautils.metrics.internal.MetricProviderImpl;
+import org.opendaylight.infrautils.testutils.LogRule;
+
+/**
+ * Unit Test for MetricProviderImpl.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class MetricProviderTest {
+
+    public @Rule LogRule logRule = new LogRule();
+
+    private final MetricProviderImpl metrics = new MetricProviderImpl();
+
+    @After
+    public void afterEachTest() {
+        metrics.close();
+    }
+
+    @Test
+    public void testMetricProviderImpl() {
+        Meter meter1 = metrics.newMeter(this, "test.meter1");
+        meter1.mark();
+        meter1.mark();
+        meter1.mark();
+        assertThat(meter1.getCount()).isEqualTo(3);
+    }
+
+    // TODO testDupeID
+
+    // TODO testBadID .. startsWith("odl") no spaces only dots
+
+}
