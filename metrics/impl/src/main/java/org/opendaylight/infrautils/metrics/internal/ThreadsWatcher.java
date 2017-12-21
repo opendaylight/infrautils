868c01d421369abe788ba5d7eecaa9f238f76480
Michael Vorburger <vorburger@redhat.com>
2017-12-21 04:25:46 +0100

    INFRAUTILS-22: Detect when more than N threads, and log details
    Change-Id: I1262d19a81c3d0f976d01f2f524fd77cb6c4b588
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/ThreadsWatcher.java
+++ metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/ThreadsWatcher.java
@@ -32,11 +32,13 @@ class ThreadsWatcher implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(ThreadsWatcher.class);
 
+    private final int maxThreads;
     private final ScheduledExecutorService scheduledExecutor;
     private final ThreadDeadlockDetector threadDeadlockDetector = new ThreadDeadlockDetector();
     private final ThreadDump threadDump = new ThreadDump(getThreadMXBean());
 
-    ThreadsWatcher(int interval, TimeUnit timeUnit) {
+    ThreadsWatcher(int maxThreads, int interval, TimeUnit timeUnit) {
+        this.maxThreads = maxThreads;
         scheduledExecutor = newSingleThreadScheduledExecutor("infrautils.metrics.ThreadsWatcher", LOG);
         addErrorLogging(scheduledExecutor.scheduleAtFixedRate(this, 0, interval, timeUnit), LOG, "scheduleAtFixedRate");
     }
@@ -47,12 +49,20 @@ class ThreadsWatcher implements Runnable {
 
     @Override
     public void run() {
+        int currentNumberOfThreads = getThreadMXBean().getThreadCount();
         Set<String> deadlockedThreadsStackTrace = threadDeadlockDetector.getDeadlockedThreads();
         if (!deadlockedThreadsStackTrace.isEmpty()) {
             LOG.error("Oh nose - there are {} deadlocked threads!! :-(", deadlockedThreadsStackTrace.size());
             for (String deadlockedThreadStackTrace : deadlockedThreadsStackTrace) {
                 LOG.error("Deadlocked thread stack trace: {}", deadlockedThreadStackTrace);
             }
+            logAllThreads();
+
+        } else if (currentNumberOfThreads >= maxThreads) {
+            LOG.warn("Oh nose - there are now {} threads, more than maximum threshold {}! "
+                    + "(totalStarted: {}, peak: {}, daemons: {})",
+                    currentNumberOfThreads, maxThreads, getThreadMXBean().getTotalStartedThreadCount(),
+                    getThreadMXBean().getPeakThreadCount(), getThreadMXBean().getDaemonThreadCount());
             logAllThreads();
         }
     }
