5329812055705ef1b04282fab66e993b703448ad
Michael Vorburger <vorburger@redhat.com>
2018-01-10 13:54:25 +0100

    Make metrics' threads deadlock watcher interval configurable
    I've manually locally tested that this works as intended.
    
    Change-Id: I01aa244fe6ed9be64bbbfa2904c9841f8b3edc12
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/Configuration.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.metrics.internal;
+
+import com.google.common.base.MoreObjects;
+import java.util.Map;
+
+/**
+ * Configuration properties for the metrics implementation.
+ *
+ * <p>Karaf's OSGi ConfigAdmin service, via the cm blueprint extension, sets this
+ * from the etc/org.opendaylight.infrautils.metrics.cfg configuration file.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class Configuration {
+
+    private final MetricProviderImpl metricProvider;
+
+    private int threadsDeadlockWatcherIntervalMS;
+
+    public Configuration(MetricProviderImpl metricProvider) {
+        this.metricProvider = metricProvider;
+    }
+
+    public void updateProperties(Map<String, String> properties) {
+        setThreadsDeadlockWatcherIntervalMS(Integer.parseInt(properties.get("threadsDeadlockWatcherIntervalMS")));
+        metricProvider.updateConfiguration(this);
+    }
+
+    public void setThreadsDeadlockWatcherIntervalMS(int ms) {
+        this.threadsDeadlockWatcherIntervalMS = ms;
+    }
+
+    public int getThreadsDeadlockWatcherIntervalMS() {
+        return this.threadsDeadlockWatcherIntervalMS;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("threadsDeadlockWatcherIntervalMS", threadsDeadlockWatcherIntervalMS)
+                .toString();
+    }
+
+}
