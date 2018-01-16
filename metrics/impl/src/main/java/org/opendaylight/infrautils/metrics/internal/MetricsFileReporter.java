a4d2d445c27313919a5d946793bd957f6f6509ea
Michael Vorburger <vorburger@redhat.com>
2018-01-17 00:15:24 +0100

    rename [Metrics]FileReporterTest and some other very minor changes
    Change-Id: I2e23af179700d8f06e810d1c27eb3fbada8de508
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/MetricsFileReporter.java
+++ metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/MetricsFileReporter.java
@@ -37,20 +37,19 @@ import org.slf4j.LoggerFactory;
 public class MetricsFileReporter extends ScheduledReporter {
 
     private static final Logger LOG = LoggerFactory.getLogger(MetricsFileReporter.class);
+
     private static final String COUNTER_FILE_PREFIX = "counter.";
     private static final String DATA_DIRECTORY = "data";
     private static final String COUNTERS_DIRECTORY = "counters";
     private static final String DEFAULT_ENCODING = "UTF-8";
-    private static final Integer METRICS_INTERVAL = 120;//TODO make it cfg variable
-    public static final String SEPERATOR = ",";
+    private static final Integer METRICS_INTERVAL = 120; // TODO make it cfg variable
+    private static final String SEPARATOR = ",";
 
-    private final MetricRegistry registry;
     private final File parentDirectory;
-    private Map<String, Long> oldCounters = new HashMap<>();
+    private final Map<String, Long> oldCounters = new HashMap<>();
 
     public MetricsFileReporter(MetricRegistry registry) {
         super(registry, "file-reporter", MetricFilter.ALL, TimeUnit.SECONDS, TimeUnit.SECONDS);
-        this.registry = registry;
         this.parentDirectory = new File(DATA_DIRECTORY, COUNTERS_DIRECTORY);
     }
 
@@ -59,7 +58,8 @@ public class MetricsFileReporter extends ScheduledReporter {
     }
 
     @Override
-    public void report(SortedMap<String, Gauge> gauges,
+    public void report(@SuppressWarnings("rawtypes")
+                       SortedMap<String, Gauge> gauges,
                        SortedMap<String, Counter> counters,
                        SortedMap<String, Histogram> histograms,
                        SortedMap<String, Meter> meters,
@@ -68,7 +68,7 @@ public class MetricsFileReporter extends ScheduledReporter {
             Calendar calendar = Calendar.getInstance();
             int hourOfTheDay = calendar.get(Calendar.HOUR_OF_DAY);
             int dayOfTheWeek = calendar.get(Calendar.DAY_OF_WEEK);
-            //retains one week worth of counters
+            // retains one week worth of counters
             rotateLastWeekFile(dayOfTheWeek, hourOfTheDay);
             boolean append = true;
             File file = createFile(dayOfTheWeek, hourOfTheDay);
@@ -78,39 +78,39 @@ public class MetricsFileReporter extends ScheduledReporter {
             pw.print(new Date());
             pw.println();
 
-            pw.println("Printing Counters");
+            pw.println("Counters:");
             for (Map.Entry<String, Counter> entry : counters.entrySet()) {
                 Counter newCounter = entry.getValue();
-                //avoid unnecessary write to report file
-                //report the counter only if there is a change
+                // avoid unnecessary write to report file
+                // report the counter only if there is a change
                 Long oldCounterObj = oldCounters.get(entry.getKey());
                 long oldCounter = oldCounterObj != null ? oldCounterObj.longValue() : 0;
                 if (newCounter.getCount() != oldCounter) {
                     pw.print(entry.getKey());
-                    printWithSeperator(pw, "count", entry.getValue().getCount());
-                    printWithSeperator(pw, "diff",
+                    printWithSeparator(pw, "count", entry.getValue().getCount());
+                    printWithSeparator(pw, "diff",
                             entry.getValue().getCount() - oldCounter);
                     pw.println();
                 }
             }
-            pw.println("Printing Gauges");
-            for (Map.Entry<String, Gauge> entry : gauges.entrySet()) {
+            pw.println("Gauges:");
+            for (@SuppressWarnings("rawtypes") Map.Entry<String, Gauge> entry : gauges.entrySet()) {
                 pw.print(entry.getKey());
                 pw.println(entry.getValue().getValue());
             }
-            pw.println("Printing Histograms");
+            pw.println("Histograms:");
             for (Map.Entry<String, Histogram> entry : histograms.entrySet()) {
                 pw.print(entry.getKey());
-                printWithSeperator(pw, "count", entry.getValue().getCount());
+                printWithSeparator(pw, "count", entry.getValue().getCount());
                 printSampling(pw, entry.getValue());
                 pw.println();
             }
-            pw.println("Printing Meters");
+            pw.println("Meters:");
             for (Map.Entry<String, Meter> entry : meters.entrySet()) {
                 pw.print(entry.getKey());
                 printMeter(pw, entry.getValue());
             }
-            pw.println("Printing Timers");
+            pw.println("Timers:");
             for (Map.Entry<String, Timer> entry : timers.entrySet()) {
                 pw.print(entry.getKey());
                 printSampling(pw, entry.getValue());
@@ -125,28 +125,28 @@ public class MetricsFileReporter extends ScheduledReporter {
 
     private static void printSampling(PrintWriter pw, Sampling sampling) {
         Snapshot snapshot = sampling.getSnapshot();
-        printWithSeperator(pw, "min", snapshot.getMin());
-        printWithSeperator(pw, "max", snapshot.getMax());
-        printWithSeperator(pw, "mean", snapshot.getMean());
+        printWithSeparator(pw, "min", snapshot.getMin());
+        printWithSeparator(pw, "max", snapshot.getMax());
+        printWithSeparator(pw, "mean", snapshot.getMean());
     }
 
     private static void printMeter(PrintWriter pw, Metered meter) {
-        printWithSeperator(pw, "count", meter.getCount());
-        printWithSeperator(pw, "oneMinuteRate", meter.getOneMinuteRate());
-        printWithSeperator(pw, "fiveMinuteRate", meter.getFiveMinuteRate());
-        printWithSeperator(pw, "fifteenMinuteRate", meter.getFifteenMinuteRate());
+        printWithSeparator(pw, "count", meter.getCount());
+        printWithSeparator(pw, "oneMinuteRate", meter.getOneMinuteRate());
+        printWithSeparator(pw, "fiveMinuteRate", meter.getFiveMinuteRate());
+        printWithSeparator(pw, "fifteenMinuteRate", meter.getFifteenMinuteRate());
         pw.println();
     }
 
-    private static void printWithSeperator(PrintWriter pw, String name, Object val) {
-        printSeperator(pw);
+    private static void printWithSeparator(PrintWriter pw, String name, Object val) {
+        printSeparator(pw);
         pw.print(name);
-        printSeperator(pw);
+        printSeparator(pw);
         pw.print(val);
     }
 
-    private static void printSeperator(PrintWriter pw) {
-        pw.print(SEPERATOR);
+    private static void printSeparator(PrintWriter pw) {
+        pw.print(SEPARATOR);
     }
 
     private static String getFileName(int dayOfTheWeek, int hourOfTheDay) {
@@ -155,16 +155,16 @@ public class MetricsFileReporter extends ScheduledReporter {
 
     public File createFile(int dayOfTheWeek, int hourOfTheDay) throws IOException {
         if (!parentDirectory.exists()) {
-            LOG.info("Directory does not exist creating one {}", parentDirectory.getName());
+            LOG.info("Directory does not exist, creating it: {}", parentDirectory.getName());
             if (!parentDirectory.mkdirs()) {
-                throw new IOException("Failed to make directories");
+                throw new IOException("Failed to make directories: " + parentDirectory.toString());
             }
         }
         File file = new File(parentDirectory, getFileName(dayOfTheWeek, hourOfTheDay));
         if (!file.exists()) {
-            LOG.info("File does not exist creating one {}", file.getName());
+            LOG.info("File does not exist, creating it: {}", file.getName());
             if (!file.createNewFile()) {
-                throw new IOException("Failed to create a file");
+                throw new IOException("Failed to create file: " + file.toString());
             }
         }
         return file;
