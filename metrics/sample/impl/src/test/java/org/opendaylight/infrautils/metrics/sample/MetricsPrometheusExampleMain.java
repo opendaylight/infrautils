8fd9a1c13eef69ab98edbb3e4fa8c5904177d101
Michael Vorburger <vorburger@redhat.com>
2018-01-24 15:01:02 +0100

    Metrics implementation based on http://Prometheus.io
    This does not yet include proper OSGi web integration with a web IT,
    Configuration and required Karaf Feature work, but is a suitable "MVP"
    for initial demos (e.g. for the ONS ODL DDF). More TBD in future
    changes; that makes this series easier to review as well.
    
    Change-Id: I7d18a413e5db71b1953529bf4103141fddf70649
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ metrics/sample/impl/src/test/java/org/opendaylight/infrautils/metrics/sample/MetricsPrometheusExampleMain.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.metrics.sample;
+
+import io.prometheus.client.exporter.HTTPServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import org.opendaylight.infrautils.metrics.prometheus.impl.PrometheusMetricProviderImpl;
+
+/**
+ * Launcher for and demo of simple standalone metrics example reporting to Prometheus.
+ *
+ * @author Michael Vorburger.ch
+ */
+public final class MetricsPrometheusExampleMain {
+
+    private MetricsPrometheusExampleMain() { }
+
+    public static void main(String[] args) throws IOException {
+        PrometheusMetricProviderImpl metricProvider = new PrometheusMetricProviderImpl();
+        MetricsExample metricsExample = new MetricsExample(metricProvider);
+        metricsExample.init();
+
+        HTTPServer server = new HTTPServer(new InetSocketAddress("localhost", 1234),
+                metricProvider.getPrometheusRegistry());
+
+        System.in.read();
+
+        server.stop();
+        metricsExample.close();
+        metricProvider.close();
+    }
+
+}
