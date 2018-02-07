49cb34ec98d36015124350af0b362e919c67704f
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2018-02-07 10:27:31 +0530

    make metrics file reporter configurable by fileReporterIntervalSecs prop
    A value of 0 on that new property means it's disabled; 0 is the default.
    
    Change-Id: I7d2f3506941877a59fe3db949aba66ec47e9c7ce
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>

--- metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/Configuration.java
+++ metrics/impl/src/main/java/org/opendaylight/infrautils/metrics/internal/Configuration.java
@@ -31,6 +31,7 @@ public final class Configuration {
     // (Just for clarity; they are commented out there, so these are the real defaults.)
     private int threadsWatcherIntervalMS = 500;
     private int maxThreads = 1000;
+    private int fileReporterIntervalSecs = 0;
 
     public Configuration(MetricProviderImpl metricProvider, Map<String, String> initialProperties) {
         this(metricProvider);
@@ -47,7 +48,17 @@ public final class Configuration {
             setThreadsWatcherIntervalMS(newThreadsWatcherIntervalMS));
         doIfIntPropertyIsPresent(properties, "maxThreads", newMaxThreads ->
             setThreadsWatcherIntervalMS(newMaxThreads));
+        doIfIntPropertyIsPresent(properties, "fileReporterIntervalSecs", newFileReporterIntervalSecs ->
+                setFileReporterIntervalSecs(newFileReporterIntervalSecs));
         metricProvider.updateConfiguration(this);
+    }
+
+    public void setFileReporterIntervalSecs(int fileReporterIntervalSecs) {
+        this.fileReporterIntervalSecs = fileReporterIntervalSecs;
+    }
+
+    public int getFileReporterIntervalSecs() {
+        return fileReporterIntervalSecs;
     }
 
     public void setThreadsWatcherIntervalMS(int ms) {
@@ -71,6 +82,7 @@ public final class Configuration {
         return MoreObjects.toStringHelper(this)
                 .add("threadsWatcherIntervalMS", threadsWatcherIntervalMS)
                 .add("maxThreads", maxThreads)
+                .add("fileReporterIntervalSecs", fileReporterIntervalSecs)
                 .toString();
     }
 
@@ -88,5 +100,4 @@ public final class Configuration {
             }
         }
     }
-
 }
