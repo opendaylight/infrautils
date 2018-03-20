00b4d3c3b4c65a9cd7b5c27ce32d7c5b274260bf
gobinath <gobinath@ericsson.com>
2018-03-20 15:16:55 +0530

    INFRAUTILS-16
    JobCoordinator enqueueJob must return a ListenableFuture instead of
    void.
    
    With this patch, the enqueueJob in JobCoordinator would return a list of
    futures which the clients(which had enqeued the jobs) could use to know
    the success/failure of each job and act on it(instead of only retrying
    the failed jobs).
    
    Change-Id: I96304fdd386a82d4f8e3648cfd3293b2ab3348bc
    Signed-off-by: gobinath <gobinath@ericsson.com>

--- jobcoordinator/jobcoordinator-api/src/main/java/org/opendaylight/infrautils/jobcoordinator/JobCoordinator.java
+++ jobcoordinator/jobcoordinator-api/src/main/java/org/opendaylight/infrautils/jobcoordinator/JobCoordinator.java
@@ -8,7 +8,6 @@
 package org.opendaylight.infrautils.jobcoordinator;
 
 import com.google.common.util.concurrent.ListenableFuture;
-
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -27,48 +26,45 @@ public interface JobCoordinator { // do *NOT* extends JobCoordinatorMonitor
     /**
      * Enqueues a job with DEFAULT_MAX_RETRIES (3) retries.
      *
-     * @param key
-     *            The job's key. Jobs with the same key are run sequentially.
-     *            Jobs with different keys are run in parallel.
-     * @param mainWorker
-     *            The task that runs for the job.
+     * @param key        The job's key. Jobs with the same key are run sequentially.
+     *                   Jobs with different keys are run in parallel.
+     * @param mainWorker The task that runs for the job.
+     * @return the result of the enqueued job.
      */
-    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker);
+    <T> List<ListenableFuture<T>> enqueueJob(String key, Callable<List<ListenableFuture<T>>> mainWorker);
 
     /**
      * Enqueues a job with a rollback task and DEFAULT_MAX_RETRIES (3) retries..
      *
-     * @param rollbackWorker
-     *            The rollback task which runs in case the job's main task
-     *            fails.
+     * @param rollbackWorker The rollback task which runs in case the job's main task
+     *                       fails.
      * @see JobCoordinator#enqueueJob(String, Callable)
      */
-    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, RollbackCallable rollbackWorker);
+    <T> List<ListenableFuture<T>> enqueueJob(String key, Callable<List<ListenableFuture<T>>> mainWorker,
+                                             RollbackCallable rollbackWorker);
 
     /**
      * Enqueues a job with max retries. In case the job's main task fails, it
      * will be retried until it succeeds or the specified maximum number of
      * retries has been reached.
      *
-     * @param maxRetries
-     *            The maximum number of retries for the job's main task until it
-     *            succeeds.
+     * @param maxRetries The maximum number of retries for the job's main task until it
+     *                   succeeds.
      * @see JobCoordinator#enqueueJob(String, Callable)
      */
-    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, int maxRetries);
+    <T> List<ListenableFuture<T>> enqueueJob(String key, Callable<List<ListenableFuture<T>>> mainWorker,
+                                             int maxRetries);
 
     /**
      * Enqueues a job with a rollback task and max retries.
      *
-     * @param rollbackWorker
-     *            The rollback task which runs in case the job's main task
-     *            fails.
-     * @param maxRetries
-     *            The maximum number of retries for the job's main task until it
-     *            succeeds.
+     * @param rollbackWorker The rollback task which runs in case the job's main task
+     *                       fails.
+     * @param maxRetries     The maximum number of retries for the job's main task until it
+     *                       succeeds.
      * @see JobCoordinator#enqueueJob(String, Callable, RollbackCallable)
      * @see JobCoordinator#enqueueJob(String, Callable, int)
      */
-    void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker, RollbackCallable rollbackWorker,
-            int maxRetries);
+    <T> List<ListenableFuture<T>> enqueueJob(String key, Callable<List<ListenableFuture<T>>> mainWorker,
+                                             RollbackCallable rollbackWorker, int maxRetries);
 }
