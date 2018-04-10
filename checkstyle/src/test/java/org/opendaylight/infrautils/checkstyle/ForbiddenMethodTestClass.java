a3468f92546f067a1701b222a1e52a2989d0805a
Stephen Kitt <skitt@redhat.com>
2018-04-10 14:27:17 +0200

    Add a Checkstyle check for forbidden methods
    This allows code to be inspected to prevent uses of certain methods.
    An example use is in https://git.opendaylight.org/gerrit/70704
    
    Change-Id: I7638a660c70988631773d87c518b230cd5089bd4
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ checkstyle/src/test/java/org/opendaylight/infrautils/checkstyle/ForbiddenMethodTestClass.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.infrautils.checkstyle;
+
+public class ForbiddenMethodTestClass {
+    public void testMethod() {
+        ForbiddenMethodTestClass.forbiddenMethod();
+        forbiddenMethod();
+    }
+
+    private static void forbiddenMethod() {
+        // Nothing to do
+    }
+}
