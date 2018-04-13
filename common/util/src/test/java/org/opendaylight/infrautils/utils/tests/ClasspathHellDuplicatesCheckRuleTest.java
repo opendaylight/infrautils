24197dc95f6646e6ffe30ac97290c259fc3b4567
Michael Vorburger <vorburger@redhat.com>
2018-04-13 19:30:08 +0200

    fix CPD problem re. ClasspathHellDuplicatesCheckRule & LogCaptureRule
    Change-Id: I3ce4bbe497652a52011443cef711605147e2f2f8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/test/java/org/opendaylight/infrautils/utils/tests/ClasspathHellDuplicatesCheckRuleTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.utils.tests;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.opendaylight.infrautils.testutils.ClasspathHellDuplicatesCheckRule;
+
+/**
+ * {@link ClasspathHellDuplicatesCheckRule} test.
+ *
+ * <p>This test is copy/pasted from the identical one in testutils,
+ * because the one there fails to find duplicates due to itself, like
+ * the StaticLoggerBinder.class in testutils for the LogCaptureRule.
+ *
+ * @author Michael Vorburger
+ */
+public class ClasspathHellDuplicatesCheckRuleTest {
+
+    public static @ClassRule ClasspathHellDuplicatesCheckRule jHades = new ClasspathHellDuplicatesCheckRule();
+
+    @Test
+    public void testSomething() throws Exception {
+        // OK!
+    }
+
+}
