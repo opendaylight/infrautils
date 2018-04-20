7a3e6d4bd996d3f576f027f4ca66205be52eb313
Michael Vorburger <vorburger@redhat.com>
2018-04-20 16:25:14 +0200

    add diagstatus.web with /diagstatus servlet
    JIRA: INFRAUTILS-33
    Change-Id: Ic763cce5455cdae5fe1653cccdaeb2020d5fabba
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ diagstatus/web/src/main/java/org/opendaylight/infrautils/diagstatus/web/DiagStatusServlet.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus.web;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.opendaylight.infrautils.diagstatus.DiagStatusService;
+
+/**
+ * Web Servlet for diagstatus which returns JSON and HTTP status code.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class DiagStatusServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    // Shut up FindBugs, because DiagStatusService is not Serializable.
+    // It's not like we'll ever run this Servlet in a stupid distribution web container
+    // which will actually serialize Servlet instances - that's so 90s WAS theoretical.
+    @SuppressFBWarnings("SE_BAD_FIELD")
+    private final DiagStatusService diagStatusService;
+
+    public DiagStatusServlet(DiagStatusService diagStatusService) {
+        this.diagStatusService = diagStatusService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(diagStatusService.getAllServiceDescriptorsAsJSON());
+        printWriter.close();
+
+        // use setStatus() NOT sendError(), because we are providing the response
+        if (!diagStatusService.isOperational()) {
+            // HTTP return code 418 instead of regular 200 is used so that scripts
+            // who just want boolean status don't have to parse the JSON, if not interested.
+            // see RFC-2324 & RFC-7168 for the meaning of HTTP return code 418 ...
+            response.setStatus(418);
+        }
+    }
+
+}
---
+++ diagstatus/web/src/main/java/org/opendaylight/infrautils/diagstatus/web/OsgiWebInitializer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.diagstatus.web;
+
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import org.opendaylight.infrautils.diagstatus.DiagStatusService;
+import org.ops4j.pax.cdi.api.OsgiService;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Initializes and registers the {@link DiagStatusServlet}.
+ *
+ * <p>This implementation uses the OSGi {@link HttpService} directly, because our
+ * WebContext and WebServer abstraction are in AAA, and we don't want to have a
+ * dependency to AAA from infrautils.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Singleton
+public class OsgiWebInitializer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OsgiWebInitializer.class);
+
+    private static final String DIAGSTATUS_URL = "/diagstatus";
+
+    private final HttpService osgiHttpService;
+
+    @Inject
+    public OsgiWebInitializer(@OsgiService HttpService osgiHttpService,
+            @OsgiService DiagStatusService diagStatusService) throws ServletException, NamespaceException {
+        this.osgiHttpService = osgiHttpService;
+        osgiHttpService.registerServlet(DIAGSTATUS_URL, new DiagStatusServlet(diagStatusService), null, null);
+        LOG.info("DiagStatus now exposed on: {}", DIAGSTATUS_URL);
+    }
+
+    @PreDestroy
+    public void close() {
+        osgiHttpService.unregister(DIAGSTATUS_URL);
+    }
+}
