7944812ef111f25431d434318c2b8c269fe46c8e
Faseela K <faseela.k@ericsson.com>
2018-06-17 10:51:53 +0530

    Enhance diagstatus ERROR message
    In case the remote JMX operation fails,
    showSvcStatus CLI is printing an ERROR which
    is not formatted properly, also the error message
    does not make much sense to the end user.
    
    Sample output given below:
    
    karaf>showSvcStatus
    
    Remote Status retrieval JMX Operation failed for node 192.168.70.3Node IP Address: 192.168.70.4
    System ready state: ACTIVE
      OPENFLOW            : OPERATIONAL
      IFM                 : OPERATIONAL
      ITM                 : OPERATIONAL
      ELAN                : OPERATIONAL
      DATASTORE           : OPERATIONAL
    
    Change-Id: I2542743cc5dc8d6d7a5879cfec17bf0c65158743
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- diagstatus/shell/src/main/java/org/opendaylight/infrautils/diagstatus/shell/DiagStatusCommand.java
+++ diagstatus/shell/src/main/java/org/opendaylight/infrautils/diagstatus/shell/DiagStatusCommand.java
@@ -58,8 +58,9 @@ public class DiagStatusCommand implements Action {
                             strBuilder.append(getRemoteStatusSummary(memberAddress));
                         }
                     } catch (Exception e) {
-                        strBuilder.append("Status retrieval JMX Operation failed for node ")
-                                .append(memberAddress);
+                        strBuilder.append("Node IP Address: ")
+                                .append(memberAddress).append(" :status retrieval failed due to ")
+                                .append(e.getMessage()).append("\n");
                         LOG.error("Exception while reaching Host {}", memberAddress, e);
                     }
                 }
