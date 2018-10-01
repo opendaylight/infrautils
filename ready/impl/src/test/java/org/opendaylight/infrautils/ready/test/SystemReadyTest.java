705551c3dcbd0e085c6ebf073ff9db648cbbc2c4
Michael Vorburger <vorburger@redhat.com>
2018-10-01 13:43:28 +0200

    rename SystemReadyImpl to KarafSystemReadyImpl
    because a (simple) SystemReadyImpl is proposed next.
    
    Change-Id: Ic4ea0cd857e4cd46f5c1a1052396eeba623abf84
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- ready/impl/src/test/java/org/opendaylight/infrautils/ready/test/SystemReadyTest.java
+++ ready/impl/src/test/java/org/opendaylight/infrautils/ready/test/SystemReadyTest.java
@@ -12,7 +12,7 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.opendaylight.infrautils.ready.SystemState;
-import org.opendaylight.infrautils.ready.internal.SystemReadyImpl;
+import org.opendaylight.infrautils.ready.internal.KarafSystemReadyImpl;
 import org.opendaylight.infrautils.testutils.LogRule;
 
 
@@ -28,7 +28,7 @@ public class SystemReadyTest {
     @Test
     public void testMbeanRegistration() throws JMException {
         // Register the SystemState MBean
-        SystemReadyImpl systemReady = new SystemReadyImpl(null, null);
+        KarafSystemReadyImpl systemReady = new KarafSystemReadyImpl(null, null);
         // Check if initial value of BOOTING is assigned after registration
         Assert.assertEquals(SystemState.BOOTING.name(), systemReady.readMBeanAttribute("SystemState"));
     }
