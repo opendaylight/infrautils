81d41b3770f82cb0ce122ac21817556e33223930
Michael Vorburger <vorburger@redhat.com>
2018-10-01 14:23:17 +0200

    factor out new SystemReadyImpl from KarafSystemReadyImpl
    so that a non-OSGi ODL distribution can provide SystemReadyMonitor (it
    would call ready() after whatever it uses for dependency injection is
    fully done).
    
    This comes out of https://github.com/vorburger/opendaylight-simple/,
    which currently has a SystemReadyBaseImpl class that is a poor man's
    version of this SystemReadyImpl; we will replace that with this.
    
    This new SystemReadyImpl also is (replaces) what I made in for that TODO
    commented out EXPLICIT Behaviour in the TestSystemReadyMonitor, which
    therefore gets removed with this.
    
    Change-Id: Icff397639f0c245b98e8e3b32dee4b3360ceaa6e
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- ready/impl/src/main/java/org/opendaylight/infrautils/ready/internal/KarafSystemReadyImpl.java
+++ ready/impl/src/main/java/org/opendaylight/infrautils/ready/internal/KarafSystemReadyImpl.java
@@ -7,18 +7,11 @@
  */
 package org.opendaylight.infrautils.ready.internal;
 
-import static org.opendaylight.infrautils.ready.SystemState.ACTIVE;
-import static org.opendaylight.infrautils.ready.SystemState.BOOTING;
 import static org.opendaylight.infrautils.ready.SystemState.FAILURE;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Throwables;
-import java.util.Optional;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
@@ -30,9 +23,8 @@ import javax.management.MalformedObjectNameException;
 import org.apache.aries.blueprint.annotation.service.Reference;
 import org.apache.aries.blueprint.annotation.service.Service;
 import org.apache.karaf.bundle.core.BundleService;
-import org.opendaylight.infrautils.ready.SystemReadyListener;
 import org.opendaylight.infrautils.ready.SystemReadyMonitor;
-import org.opendaylight.infrautils.ready.SystemState;
+import org.opendaylight.infrautils.ready.spi.SystemReadyImpl;
 import org.opendaylight.infrautils.ready.spi.SystemReadyMonitorMXBeanImpl;
 import org.opendaylight.infrautils.utils.concurrent.ThreadFactoryProvider;
 import org.opendaylight.odlparent.bundlestest.lib.SystemStateFailureException;
@@ -42,20 +34,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Implementation of the "system ready" service for an OSGi Karaf environment.
+ * {@link SystemReadyMonitor} implementation for an OSGi Karaf environment.
  *
  * @author Michael Vorburger.ch
  * @author Faseela K
  */
 @Singleton
 @Service(classes = SystemReadyMonitor.class)
-public class KarafSystemReadyImpl implements SystemReadyMonitor, Runnable {
+public class KarafSystemReadyImpl extends SystemReadyImpl implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(KarafSystemReadyImpl.class);
-
-    private final Queue<SystemReadyListener> listeners = new ConcurrentLinkedQueue<>();
-    private final AtomicReference<SystemState> currentSystemState = new AtomicReference<>(BOOTING);
-    private final AtomicReference<Throwable> currentSystemFailureCause = new AtomicReference<>();
 
     private final ThreadFactory threadFactory = ThreadFactoryProvider.builder()
                                                     .namePrefix("SystemReadyService")
@@ -97,77 +85,22 @@ public class KarafSystemReadyImpl implements SystemReadyMonitor, Runnable {
                     // INFRAUTILS-17: getSummaryText() instead getFullDiagnosticText() because ppl found log confusing
                     bundleDiagInfos.getSummaryText()));
 
-            SystemReadyListener[] toNotify;
-            synchronized (listeners) {
-                toNotify = listeners.toArray(new SystemReadyListener[listeners.size()]);
-                currentSystemState.set(ACTIVE);
-            }
-            LOG.info("System ready; AKA: Aye captain, all warp coils are now operating at peak efficiency! [M.]");
-
-            if (toNotify.length > 0) {
-                LOG.info("Now notifying all its registered SystemReadyListeners...");
-            }
-
-            for (SystemReadyListener element : toNotify) {
-                element.onSystemBootReady();
-            }
-
         } catch (SystemStateFailureException e) {
             LOG.error("Failed, some bundles did not start (SystemReadyListeners are not called)", e);
-            currentSystemState.set(FAILURE);
-            currentSystemFailureCause.set(e);
+            setSystemState(FAILURE);
+            setSystemFailureCause(e);
             // We do not re-throw this
 
         } catch (RuntimeException throwable) {
             // It's exceptionally OK to catch RuntimeException here,
             // because we do want to set the currentFullSystemStatus
             LOG.error("Boot failed; not all SystemReadyListeners were not called, SystemState FAILURE", throwable);
-            currentSystemState.set(FAILURE);
-            currentSystemFailureCause.set(throwable);
+            setSystemState(FAILURE);
+            setSystemFailureCause(throwable);
             // and now we do re-throw it!
             throw throwable;
-        } catch (Exception e) {
-            LOG.error("SystemReadyListener.onSystemBootReady() threw Exception; "
-                    + "other SystemReadyListeners not called; SystemState FAILURE", e);
-            currentSystemState.set(FAILURE);
-            currentSystemFailureCause.set(e);
-            LOG.error("Failed unexpectedly (SystemReadyListeners are not called)", e);
-            // really no point in re-throwing it
-        }
-    }
-
-    @Override
-    public SystemState getSystemState() {
-        return currentSystemState.get();
-    }
-
-    @Override
-    public String getFailureCause() {
-        return Optional.ofNullable(currentSystemFailureCause.get())
-                .map(throwable -> Throwables.getStackTraceAsString(throwable)).orElse("");
-    }
-
-    @Override
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    public void registerListener(SystemReadyListener listener) {
-        SystemState state;
-        synchronized (listeners) {
-            state = currentSystemState.get();
-            if (state == BOOTING) {
-                listeners.add(listener);
-            }
         }
 
-        if (state == ACTIVE) {
-            try {
-                listener.onSystemBootReady();
-            } catch (Exception e) {
-                LOG.error("SystemReadyListener.onSystemBootReady() threw Exception; "
-                        + "but state was already ACTIVE, going back to FAILURE now", e);
-                currentSystemState.set(FAILURE);
-                currentSystemFailureCause.set(e);
-                // Do not re-throw.
-            }
-        }
+        ready();
     }
 }
