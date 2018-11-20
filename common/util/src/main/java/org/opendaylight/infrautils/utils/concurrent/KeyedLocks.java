36ca542a4da517bf182c2b15e917d82bfb4c314e
Robert Varga <robert.varga@pantheon.tech>
2018-11-20 15:46:43 +0100

    WIP: rework KeyedLocks
    Use GC to free unused locks and wrap each acquired instance
    with a AutoCloseable.
    
    Change-Id: I6a86cd22ddc6b961992069fbaf322ed5f79fe050
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/util/src/main/java/org/opendaylight/infrautils/utils/concurrent/KeyedLocks.java
+++ common/util/src/main/java/org/opendaylight/infrautils/utils/concurrent/KeyedLocks.java
@@ -7,39 +7,130 @@
  */
 package org.opendaylight.infrautils.utils.concurrent;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.annotations.VisibleForTesting;
-import java.util.HashMap;
+import com.google.common.base.FinalizableReferenceQueue;
+import com.google.common.base.FinalizableWeakReference;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.concurrent.locks.ReentrantLock;
-import javax.annotation.Nonnull;
-import javax.annotation.concurrent.GuardedBy;
+import org.eclipse.jdt.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Manages multiple ReentrantLocks by key.
  *
+ * <p>
+ * Example of use of lock():
+ * <pre>
+ *     KeyedLocks&lt;String&&gt; locks;
+ *     String lockName;
+ *
+ *     try (AcquiredLock lock = locks.lock(lockName)) {
+ *          // locked region
+ *     }
+ *
+ *     // lock released
+ * </pre>
+ *
+ * <p>
+ * Example of use of tryLock():
+ * <pre>
+ *     KeyedLocks&lt;String&gt; locks;
+ *     String lockName;
+ *
+ *     try (LockResult lock = locks.tryLock(lockName)) {
+ *         // maybe locked region
+ *
+ *         if (lock.wasAcquired()) {
+ *             // locked region
+ *         }
+ *     });
+ *
+ *     // lock released
+ * </pre>
+ *
  * @param <T> the key type
  * @author Thomas Pantelis
  */
 public class KeyedLocks<T> {
-    @FunctionalInterface
-    private interface BooleanLockFunction<T> {
-        boolean apply(T key, CountingReentrantLock lock);
+    public abstract static class LockResult implements AutoCloseable {
+        public abstract boolean wasAcquired();
+
+        @Override
+        public abstract void close();
     }
 
-    private static class CountingReentrantLock extends ReentrantLock {
-        private static final long serialVersionUID = 1;
+    public static final class Acquired extends LockResult {
+        @SuppressWarnings("null")
+        private static final AtomicReferenceFieldUpdater<Acquired, @Nullable ReentrantLock> UPDATER =
+                (AtomicReferenceFieldUpdater<Acquired, @Nullable ReentrantLock>)
+                AtomicReferenceFieldUpdater.newUpdater(Acquired.class, ReentrantLock.class, "lock");
 
-        final AtomicInteger useCount = new AtomicInteger(1);
+        @SuppressWarnings("unused")
+        private volatile @Nullable ReentrantLock lock;
+
+        Acquired(ReentrantLock lock) {
+            this.lock = requireNonNull(lock);
+        }
+
+        @Override
+        public boolean wasAcquired() {
+            return true;
+        }
+
+        @Override
+        public void close() {
+            // This is quite a costly guard, alternative is:
+            //    lock.unlock();
+            //    lock = null;
+            // i.e. throwing a NPE if weirdness happens
+            ReentrantLock local = UPDATER.getAndSet(this, null);
+            if (local != null) {
+                local.unlock();
+            } else {
+                LOG.warn("Attempted to perform another unlock", new Throwable());
+            }
+        }
+    }
+
+    private static final class NotAcquired extends LockResult {
+        @Override
+        public boolean wasAcquired() {
+            return false;
+        }
+
+        @Override
+        public void close() {
+            // Intentional no-op
+        }
+    }
+
+    private static final class WeakRef<T> extends FinalizableWeakReference<ReentrantLock> {
+        private final Map<T, WeakRef<T>> locks;
+        private final T key;
+
+        WeakRef(FinalizableReferenceQueue queue, Map<T, WeakRef<T>> locks, T key) {
+            super(new ReentrantLock(), queue);
+            this.locks = requireNonNull(locks);
+            this.key = requireNonNull(key);
+        }
+
+        @Override
+        public void finalizeReferent() {
+            locks.remove(key, this);
+        }
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(KeyedLocks.class);
+    private static final NotAcquired NOT_ACQUIRED = new NotAcquired();
 
-    @GuardedBy("locks")
-    private final Map<T, CountingReentrantLock> locks = new HashMap<>();
+    private final FinalizableReferenceQueue queue = new FinalizableReferenceQueue();
+    private final Map<T, WeakRef<T>> locks = new ConcurrentHashMap<>();
 
     /**
      * Tries to acquire the lock for the given key if it is not held by another thread within the given waiting time.
@@ -52,23 +143,19 @@ public class KeyedLocks<T> {
      *         by the current thread, otherwise {@code false} if the waiting time elapsed before the lock could be
      *         acquired or the current thread was interrupted.
      */
-    public boolean tryLock(@Nonnull T lockKey, long timeout, TimeUnit unit) {
+    public LockResult tryLock(T lockKey, long timeout, TimeUnit unit) {
         LOG.debug("tryLock {}, time {}, unit: {}", lockKey, timeout, unit);
-        return doLock(lockKey, (key, lock) -> {
-            try {
-                boolean locked = lock.tryLock(timeout, unit);
-                if (!locked) {
-                    lock.useCount.decrementAndGet();
-                    LOG.debug("tryLock {} - already locked - count: {}", key, lock.useCount);
-                }
 
-                return locked;
-            } catch (InterruptedException e) {
-                lock.useCount.decrementAndGet();
-                LOG.warn("tryLock was interrrupted for key {} - remaining use count: {}", key, lock.useCount);
-                return false;
-            }
-        });
+        ReentrantLock lock = getLock(lockKey);
+        boolean locked;
+        try {
+            locked = lock.tryLock(timeout, unit);
+        } catch (InterruptedException e) {
+            LOG.warn("tryLock was interrrupted for key {}", lockKey, e);
+            return NOT_ACQUIRED;
+        }
+
+        return locked ? new Acquired(lock) : NOT_ACQUIRED;
     }
 
     /**
@@ -79,17 +166,10 @@ public class KeyedLocks<T> {
      * @return {@code true} if the lock was free and was acquired by the current thread, or the lock was already held
      *         by the current thread, otherwise {@code false} otherwise
      */
-    public boolean tryLock(@Nonnull T lockKey) {
+    public LockResult tryLock(T lockKey) {
         LOG.debug("tryLock {}", lockKey);
-        return doLock(lockKey, (key, lock) -> {
-            boolean locked = lock.tryLock();
-            if (!locked) {
-                lock.useCount.decrementAndGet();
-                LOG.debug("tryLock {} - already locked - count: {}", key, lock.useCount);
-            }
-
-            return locked;
-        });
+        ReentrantLock lock = getLock(lockKey);
+        return lock.tryLock() ? new Acquired(lock) : NOT_ACQUIRED;
     }
 
     /**
@@ -98,63 +178,29 @@ public class KeyedLocks<T> {
      *
      * @param lockKey the key to lock
      */
-    public void lock(@Nonnull T lockKey) {
+    public Acquired lock(T lockKey) {
         LOG.debug("lock {}", lockKey);
-        doLock(lockKey, (key, lock) -> {
-            lock.lock();
-            return true;
-        });
+        ReentrantLock lock = getLock(lockKey);
+        lock.lock();
+        return new Acquired(lock);
     }
 
-    private boolean doLock(T lockKey, BooleanLockFunction<T> lockFunction) {
-        CountingReentrantLock lock;
-        synchronized (locks) {
-            lock = locks.get(lockKey);
-            if (lock == null) {
-                LOG.debug("Creating new Lock for {}", lockKey);
-                CountingReentrantLock newLock = new CountingReentrantLock();
-                locks.put(lockKey, newLock);
-                return lockFunction.apply(lockKey, newLock);
-            } else {
-                lock.useCount.incrementAndGet();
-                LOG.debug("Lock for {} aleady exists - new count {}", lockKey, lock.useCount);
-            }
-        }
-
-        return lockFunction.apply(lockKey, lock);
-    }
-
-    /**
-     * Attempts to release the lock for the given key.
-     * See {@link ReentrantLock#unlock()} for more details.
-     *
-     * @param lockKey the key to unlock
-     * @throws IllegalMonitorStateException if the current thread does not hold this lock
-     */
-    public void unlock(@Nonnull T lockKey) {
-        LOG.debug("unlock {}", lockKey);
-        CountingReentrantLock lock;
-        synchronized (locks) {
-            lock = locks.get(lockKey);
+    // This might be useful for advanced users, who use this just as a T -> ReentrantLock cache
+    private ReentrantLock getLock(T lockKey) {
+        while (true) {
+            WeakRef<T> ref = locks.computeIfAbsent(lockKey, key -> new WeakRef<>(queue, locks, key));
+            ReentrantLock lock = ref.get();
             if (lock != null) {
-                LOG.debug("Found Lock for {} - count: {}", lockKey, lock.useCount);
-                if (lock.useCount.decrementAndGet() <= 0) {
-                    LOG.debug("Rmoving Lock for {}", lockKey);
-                    locks.remove(lockKey);
-                }
-            } else {
-                throw new IllegalMonitorStateException("Lock for key " + lockKey
-                        + " is not owned by the current thread");
+                return lock;
             }
-        }
 
-        lock.unlock();
+            // The lock is gone, retry
+            locks.remove(lockKey, ref);
+        }
     }
 
     @VisibleForTesting
-    int size() {
-        synchronized (locks) {
-            return locks.size();
-        }
+    long size() {
+        return locks.size();
     }
 }
