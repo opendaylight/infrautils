d0aa7b89797ba89a77fe9a08b9ebb5c78708e5e3
Faseela K <faseela.k@ericsson.com>
2018-12-16 19:42:37 +0530

    Fixing verify build issues in infrautils
    Change-Id: I42f56611b693355fd132fefaf694cd86a97ec52e
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ testutils/src/test/java/org/opendaylight/infrautils/testutils/tests/ClasspathHellDuplicatesCheckerTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.testutils.tests;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.opendaylight.infrautils.testutils.ClasspathHellDuplicatesChecker;
+
+/**
+ * Unit test for {@link ClasspathHellDuplicatesChecker}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class ClasspathHellDuplicatesCheckerTest {
+
+    @Test
+    public void testNoDuplicates() {
+        assertThat(new ClasspathHellDuplicatesChecker().getDuplicates()).isEmpty();
+    }
+
+    @Test
+    public void testDuplicates() {
+        // Just to make sure that is actually does something, if we were not to filter anything, there would be some:
+        assertThat(new ClasspathHellDuplicatesChecker() {
+            @Override
+            protected boolean isHarmlessDuplicate(String resourcePath) {
+                return false;
+            }
+        }.getDuplicates()).isNotEmpty();
+    }
+}
