304486aaca9e7611a8b5a866dc50a07d70204555
Michael Vorburger <vorburger@redhat.com>
2018-12-22 04:09:48 +0100

    introduce inject.guice.AutoWiringModule
    It lets tests use classpath scanning based "auto-wiring" (à la Spring).
    
    This comes out of https://github.com/vorburger/opendaylight-simple
    
    Change-Id: I0c71be4930ec3158c1fb8213913fe693d4ccdec3
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ inject/inject-guice/src/test/java/org/opendaylight/infrautils/inject/guice/test/ClassPathScannerTestNoInterfacesImplementation.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject.guice.test;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class ClassPathScannerTestNoInterfacesImplementation {
+}
