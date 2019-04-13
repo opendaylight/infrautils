bfdfd92e01285dc3f01bd49f4cd3682fa6f02999
Robert Varga <robert.varga@pantheon.tech>
2019-04-13 12:10:50 +0200

    Improve NamedLocks performance
    NamedLocks is performance-critical, make sure we allow interactions
    with backing map to be devirtualized as soon as possible.
    
    Furthermore we peerform simple atomic operations instead of
    computeIfAbsent(), which always acquires locks to guarantee the
    computation occurs at most once -- which we don't care about
    anyway.
    
    Change-Id: Id6e34a609931e30a05194fcf41a97f13ab6a932a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/util/src/test/java/org/opendaylight/infrautils/utils/concurrent/NamedLocksTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.utils.concurrent;
+
+import static org.junit.Assert.assertSame;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.infrautils.utils.concurrent.NamedSimpleReentrantLock.Acquired;
+
+public class NamedLocksTest {
+    private @Nullable NamedLocks<String> locks;
+
+    @Before
+    public void before() {
+        locks = new NamedLocks<>();
+    }
+
+    @Test
+    public void testLookup() {
+        NamedSimpleReentrantLock<String> first = locks.getLock("foo");
+        NamedSimpleReentrantLock<String> second = locks.getLock("foo");
+        assertSame(first, second);
+    }
+
+    @Test
+    public void testMultiAcquire() {
+        Acquired first = locks.acquire("foo");
+        Acquired second = locks.acquire("foo");
+        first.close();
+        second.close();
+    }
+}
