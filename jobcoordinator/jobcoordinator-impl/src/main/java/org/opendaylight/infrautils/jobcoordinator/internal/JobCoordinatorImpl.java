984e78e8ffe25bbf576503301d5d4b5d85646bd6
A Vamsikrishna <a.vamsikrishna@ericsson.com>
2019-08-23 15:06:28 +0530

    This review includes the JC metrics such as:
    DJC Queue Status.
    Pending Jobs Count
    Finished Jobs Count
    Moving Average of Jobs completed
    
    Signed-off-by: A Vamsikrishna <a.vamsikrishna@ericsson.com>
    Change-Id: I53573082ca157ddd35c345142dbe41015dcbe429

--- jobcoordinator/jobcoordinator-impl/src/main/java/org/opendaylight/infrautils/jobcoordinator/internal/JobCoordinatorImpl.java
+++ jobcoordinator/jobcoordinator-impl/src/main/java/org/opendaylight/infrautils/jobcoordinator/internal/JobCoordinatorImpl.java
@@ -19,6 +19,7 @@ import com.google.errorprone.annotations.Var;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -29,8 +30,10 @@ import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -43,10 +46,8 @@ import org.opendaylight.infrautils.jobcoordinator.RollbackCallable;
 import org.opendaylight.infrautils.metrics.Counter;
 import org.opendaylight.infrautils.metrics.Meter;
 import org.opendaylight.infrautils.metrics.MetricProvider;
-import org.opendaylight.infrautils.utils.ClassLoaders;
 import org.opendaylight.infrautils.utils.concurrent.JdkFutures;
 import org.opendaylight.infrautils.utils.concurrent.LoggingThreadUncaughtExceptionHandler;
-import org.opendaylight.infrautils.utils.concurrent.LoggingUncaughtThreadDeathContextRunnable;
 import org.opendaylight.infrautils.utils.concurrent.ThreadFactoryProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,9 +61,11 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
     private static final long RETRY_WAIT_BASE_TIME_MILLIS = 1000;
 
     private static final int FJP_MAX_CAP = 0x7fff; // max #workers - 1; copy/pasted from ForkJoinPool private
+
+    private static final AtomicInteger THREAD_INDEX = new AtomicInteger(0);
     private final ForkJoinPool.ForkJoinWorkerThreadFactory factory = pool -> {
         ForkJoinWorkerThread worker = ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool);
-        worker.setName("jobcoordinator-main-task-" + worker.getPoolIndex());
+        worker.setName("jobcoordinator-main-task-" + THREAD_INDEX.getAndIncrement());
         return worker;
     };
     private final ForkJoinPool fjPool = new ForkJoinPool(
@@ -150,10 +153,17 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
     @Override
     public void enqueueJob(String key, Callable<List<ListenableFuture<Void>>> mainWorker,
             RollbackCallable rollbackWorker, int maxRetries) {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        JobEntry jobEntry = new JobEntry(key, mainWorker, rollbackWorker, maxRetries, classLoader);
-        JobQueue jobQueue = jobQueueMap.computeIfAbsent(key, mapKey -> new JobQueue());
-        jobQueue.addEntry(jobEntry);
+
+        jobQueueMapLock.lock();
+        try {
+            JobQueue jobQueue = jobQueueMap.computeIfAbsent(key, mapKey -> new JobQueue());
+            JobEntry jobEntry = new JobEntry(key, jobQueue.getQueueId(), mainWorker, rollbackWorker, maxRetries);
+            jobQueue.addEntry(jobEntry);
+            LOG.trace("Added a job with key {}, job {} to the queue {}",
+                    key, jobEntry.getId(), jobQueue.getQueueId());
+        } finally {
+            jobQueueMapLock.unlock();
+        }
 
         jobsPending.increment();
         jobsIncomplete.increment();
@@ -197,10 +207,26 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
      **/
     private void clearJob(JobEntry jobEntry) {
         String jobKey = jobEntry.getKey();
-        LOG.trace("About to clear jobKey: {}", jobKey);
+        LOG.trace("About to clear job with key {}, job{} from queue {}",
+                jobKey, jobEntry.getId(), jobEntry.getQueueId());
         JobQueue jobQueue = jobQueueMap.get(jobKey);
         if (jobQueue != null) {
+
+
+            jobQueueMapLock.lock();
+            try {
+                if (jobQueue.isEmpty()) {
+                    jobQueueMap.remove(jobKey);
+                    LOG.trace("Removed jobQueue {} for jobKey {} while clearing job {}",
+                            jobQueue.getQueueId(), jobKey, jobEntry.getId());
+                }
+            } finally {
+                jobQueueMapLock.unlock();
+            }
+            jobEntry.setEndTime(System.currentTimeMillis());
+            jobQueue.onJobFinished(jobEntry.getEndTime() - jobEntry.getStartTime());
             jobQueue.setExecutingEntry(null);
+
         } else {
             LOG.error("clearJob: jobQueueMap did not contain the key for this entry: {}", jobEntry);
         }
@@ -223,9 +249,9 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
         }
     }
 
-    private boolean executeTask(Runnable task, ClassLoader classLoader) {
+    private boolean executeTask(Runnable task) {
         try {
-            fjPool.execute(ClassLoaders.wrap(task, classLoader));
+            fjPool.execute(task);
             return true;
         } catch (RejectedExecutionException e) {
             if (!fjPool.isShutdown()) {
@@ -270,7 +296,8 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
          */
         @Override
         public void onSuccess(List<Void> voids) {
-            LOG.trace("Job completed successfully: {}", jobEntry.getKey());
+            LOG.trace("Job completed successfully with key {}, job {} from queue {}",
+                    jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
             clearJob(jobEntry);
         }
 
@@ -305,7 +332,7 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
                 long waitTime = RETRY_WAIT_BASE_TIME_MILLIS / retryCount;
                 Futures.addCallback(JdkFutures.toListenableFuture(scheduleTask(() -> {
                     MainTask worker = new MainTask(jobEntry);
-                    executeTask(worker, jobEntry.getContextClassLoader());
+                    executeTask(worker);
                 }, waitTime, TimeUnit.MILLISECONDS)), new FutureCallback<Object>() {
                     @Override
                     public void onFailure(Throwable throwable) {
@@ -315,7 +342,8 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
 
                     @Override
                     public void onSuccess(Object result) {
-                        LOG.debug("Retry of job succeeded: {}", jobEntry);
+                        LOG.debug("Retry of job submission succeeded with key {}, job {} from queue {}",
+                                jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
                     }
                 }, MoreExecutors.directExecutor());
             } else {
@@ -329,7 +357,7 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
         if (jobEntry.getRollbackWorker() != null) {
             jobEntry.setMainWorker(null);
             RollbackTask rollbackTask = new RollbackTask(jobEntry);
-            executeTask(rollbackTask, jobEntry.getContextClassLoader());
+            executeTask(rollbackTask);
             return;
         }
         clearJob(jobEntry);
@@ -339,23 +367,22 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
      * RollbackTask is used to execute the RollbackCallable provided by the
      * application in the eventuality of a failure.
      */
-    private class RollbackTask extends LoggingUncaughtThreadDeathContextRunnable {
+    private class RollbackTask implements Runnable {
         private final JobEntry jobEntry;
 
         RollbackTask(JobEntry jobEntry) {
-            super(LOG, jobEntry::toString);
             this.jobEntry = jobEntry;
         }
 
         @Override
         @SuppressWarnings("checkstyle:IllegalCatch")
-        public void runWithUncheckedExceptionLogging() {
+        public void run() {
             RollbackCallable rollbackWorker = jobEntry.getRollbackWorker();
             @Var List<ListenableFuture<Void>> futures = null;
             if (rollbackWorker != null) {
                 try {
                     futures = rollbackWorker.apply(jobEntry.getFutures());
-                } catch (Exception e) {
+                } catch (Throwable e) {
                     LOG.error("Exception when executing job's rollbackWorker: {}", jobEntry, e);
                 }
             } else {
@@ -363,6 +390,8 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
             }
 
             if (futures == null || futures.isEmpty()) {
+                LOG.trace("From RollbackTask: futures is null or empty. Clearing the jobQueue with key {},"
+                        + " job {} from queue {}", jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
                 clearJob(jobEntry);
                 return;
             }
@@ -376,37 +405,49 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
     /**
      * Execute the MainWorker callable.
      */
-    private class MainTask extends LoggingUncaughtThreadDeathContextRunnable {
+    private class MainTask implements Runnable {
         private static final int LONG_JOBS_THRESHOLD_MS = 1000;
         private final JobEntry jobEntry;
 
         MainTask(JobEntry jobEntry) {
-            super(LOG, jobEntry::toString);
             this.jobEntry = jobEntry;
         }
 
         @Override
         @SuppressWarnings("checkstyle:illegalcatch")
-        public void runWithUncheckedExceptionLogging() {
+        public void run() {
             @Var List<ListenableFuture<Void>> futures = null;
             long jobStartTimestampNanos = System.nanoTime();
-            LOG.trace("Running job with key: {}", jobEntry.getKey());
+            jobEntry.setStartTime(System.currentTimeMillis());
+            LOG.trace("Running job with key {}, job {} from queue {}",
+                    jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
 
             try {
                 Callable<List<ListenableFuture<Void>>> mainWorker = jobEntry.getMainWorker();
                 if (mainWorker != null) {
-                    futures = ClassLoaders.call(mainWorker, jobEntry.getContextClassLoader());
+                    futures = mainWorker.call();
                 } else {
                     LOG.error("Unexpected no (null) main worker on job: {}", jobEntry);
                 }
                 long jobExecutionTimeNanos = System.nanoTime() - jobStartTimestampNanos;
                 printJobs(jobEntry.getKey(), TimeUnit.NANOSECONDS.toMillis(jobExecutionTimeNanos));
-            } catch (Exception e) {
+            } catch (Throwable e) {
                 jobsFailed.mark();
                 LOG.error("Direct Exception (not failed Future) when executing job, won't even retry: {}", jobEntry, e);
             }
 
             if (futures == null || futures.isEmpty()) {
+                LOG.trace("From MainTask: futures is null or empty. Clearing the jobQueue with key {},"
+                        + " job {} from queue {}", jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
+                clearJob(jobEntry);
+                return;
+            }
+
+            List<ListenableFuture<Void>> nonNullFutures = futures.stream().filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+            if (nonNullFutures.isEmpty()) {
+                LOG.trace("From MainTask nonNullFutures: Clearing the jobQueue with key {}, job {} from queue {}",
+                        jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
                 clearJob(jobEntry);
                 return;
             }
@@ -418,10 +459,12 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
 
         private void printJobs(String key, long jobExecutionTime) {
             if (jobExecutionTime > LONG_JOBS_THRESHOLD_MS) {
-                LOG.warn("Job {} took {}ms to complete", key, jobExecutionTime);
+                LOG.warn("Job with key {}, job {} from queue {} took {}ms to complete",
+                        jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId(), jobExecutionTime);
                 return;
             }
-            LOG.trace("Job {} took {}ms to complete", key, jobExecutionTime);
+            LOG.trace("Job with key {}, job {} from queue {} took {}ms to complete",
+                    jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId(), jobExecutionTime);
         }
     }
 
@@ -448,9 +491,10 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
                         }
                         jobQueue.setExecutingEntry(jobEntry);
                         MainTask worker = new MainTask(jobEntry);
-                        LOG.trace("Executing job with key: {}", jobEntry.getKey());
+                        LOG.trace("Executing job with key {}, job {} from queue {}",
+                                jobEntry.getKey(), jobEntry.getId(), jobEntry.getQueueId());
 
-                        if (executeTask(worker, jobEntry.getContextClassLoader())) {
+                        if (executeTask(worker)) {
                             jobsPending.decrement();
                         }
                     }
@@ -458,7 +502,7 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
                     if (!waitForJobIfNeeded()) {
                         break;
                     }
-                } catch (Exception e) {
+                } catch (Throwable e) {
                     LOG.error("Exception while executing the tasks", e);
                 }
             }
@@ -479,6 +523,11 @@ public class JobCoordinatorImpl implements JobCoordinator, JobCoordinatorMonitor
                 jobQueueMapLock.unlock();
             }
         }
+    }
+
+
+    public Map<String, JobQueue> getJobQueueMap() {
+        return jobQueueMap;
     }
 
     @Override
