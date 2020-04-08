fa66173d9cbaaa58df81df9f19a2113a26ab68e1
dheenadayalan b <dhinua@gmail.com>
2020-04-08 12:10:06 +0530

    Migration of Http client utilities from diagstatus shell to diagstatus api
    description: Migration is necessary as the infrautil-artifacts has no reference to diagstatus shell
    
    Signed-off-by: dheenadayalan b <dhinua@gmail.com>
    Change-Id: Ib3512f52b3c223c8053127482938efb224759a3a
    Signed-off-by: dheenadayalan b <dhinua@gmail.com>

--- diagstatus/api/src/main/java/org/opendaylight/infrautils/diagstatus/HttpRequest.java
+++ diagstatus/api/src/main/java/org/opendaylight/infrautils/diagstatus/HttpRequest.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.infrautils.diagstatus.shell;
+package org.opendaylight.infrautils.diagstatus;
 
 import java.util.List;
 import java.util.Map;
