db28bb01ca4d7d68f95e20506ba8b407a0befe84
Robert Varga <robert.varga@pantheon.tech>
2020-09-24 00:19:28 +0200

    Fix a few EP complaints
    EP is noisy about imports, make it a bit happier.
    
    Change-Id: I5aade452864176970d673b6888caf9d913b0446c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- testutils/src/main/java/org/opendaylight/infrautils/testutils/package-info.java
+++ testutils/src/main/java/org/opendaylight/infrautils/testutils/package-info.java
@@ -5,5 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-@org.eclipse.jdt.annotation.NonNullByDefault
+@NonNullByDefault
 package org.opendaylight.infrautils.testutils;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
