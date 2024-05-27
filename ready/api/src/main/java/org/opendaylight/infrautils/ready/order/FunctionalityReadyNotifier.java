a34e2f1e2ea0454a5c1f6d775bce6a8e04150239
Robert Varga <robert.varga@pantheon.tech>
2024-05-27 21:05:08 +0200

    Deprecate o.o.i.ready.order
    This interface is currently unused and its only implementation does
    exactly what one would do for a ComponentFactory.
    
    // FIXME: file an issue
    
    Change-Id: Iacb03c8bfef876b71fe88ed743e4e01906c0d723
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- ready/api/src/main/java/org/opendaylight/infrautils/ready/order/FunctionalityReadyNotifier.java
+++ ready/api/src/main/java/org/opendaylight/infrautils/ready/order/FunctionalityReadyNotifier.java
@@ -11,9 +11,11 @@ package org.opendaylight.infrautils.ready.order;
  * Service to register {@link FunctionalityReady} markers.
  *
  * @author Michael Vorburger.ch
+ * @deprecated Callers of {@link #register(Class)} should be refactored to call {@code ComponentFactory.newInstance()}
+ *             instead.
  */
+@Deprecated(since = "6.0.9", forRemoval = true)
 public interface FunctionalityReadyNotifier {
-
     /**
      * Register a {@link FunctionalityReady} marker.
      *
@@ -21,9 +23,7 @@ public interface FunctionalityReadyNotifier {
      *            the marker FunctionalityReady marker interface
      * @return the {@link FunctionalityReadyRegistration} which allows to
      *         {@link FunctionalityReadyRegistration#unregister()} the marker
-     *
      * @see FunctionalityReady
      */
     <T extends FunctionalityReady> FunctionalityReadyRegistration<T> register(Class<T> markerInterface);
-
 }
