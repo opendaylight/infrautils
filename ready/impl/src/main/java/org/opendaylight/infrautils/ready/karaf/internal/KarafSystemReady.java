ae4a2f12a1505ab3a1693ed1f98d9bd4ae11e9f5
Robert Varga <robert.varga@pantheon.tech>
2025-02-09 00:35:44 +0100

    Inline TestBundleDiag
    KarafSystemReady is the only user, making this class a single-method
    encapsulation. Remove that encapsulation.
    
    JIRA: INFRAUTILS-104
    Change-Id: I93f88d68763e2f1ff4d932a7276909e7fc788a6d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- ready/impl/src/main/java/org/opendaylight/infrautils/ready/karaf/internal/KarafSystemReady.java
+++ ready/impl/src/main/java/org/opendaylight/infrautils/ready/karaf/internal/KarafSystemReady.java
@@ -7,12 +7,14 @@
  */
 package org.opendaylight.infrautils.ready.karaf.internal;
 
+import static java.util.Objects.requireNonNull;
 import static org.opendaylight.infrautils.ready.SystemState.FAILURE;
 
 import com.google.common.annotations.VisibleForTesting;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 import org.opendaylight.infrautils.ready.SystemReadyMonitor;
 import org.opendaylight.infrautils.ready.spi.DelegatingSystemReadyMonitorMXBean;
 import org.opendaylight.infrautils.ready.spi.SimpleSystemReadyMonitor;
@@ -55,13 +57,13 @@ public final class KarafSystemReady extends SimpleSystemReadyMonitor {
     private final DelegatingSystemReadyMonitorMXBean mbean = new DelegatingSystemReadyMonitorMXBean(this);
 
     private final Config config;
-    private final TestBundleDiag testBundleDiag;
+    private final DiagProvider diagProvider;
 
     @Activate
     public KarafSystemReady(@Reference DiagProvider diagProvider, Config newConfig) {
         this.config = newConfig;
         mbean.registerMBean();
-        testBundleDiag = new TestBundleDiag(diagProvider);
+        this.diagProvider = requireNonNull(diagProvider);
         LOG.info("Now starting to provide full system readiness status updates (see TestBundleDiag's logs)...");
         threadFactory.newThread(this::runCheckBundleDiag).start();
     }
@@ -71,12 +73,17 @@ public final class KarafSystemReady extends SimpleSystemReadyMonitor {
         this.mbean.unregisterMBean();
     }
 
+    @VisibleForTesting
+    DelegatingSystemReadyMonitorMXBean getMbean() {
+        return mbean;
+    }
+
     @SuppressWarnings("checkstyle:IllegalCatch") // below
     @SuppressFBWarnings(value = "THROWS_METHOD_THROWS_RUNTIMEEXCEPTION", justification = "Re-thrown")
     private void runCheckBundleDiag() {
         try {
             // 5 minutes really ought to be enough for the whole circus to completely boot up?!
-            testBundleDiag.checkBundleDiagInfos(config.systemReadyTimeout(), TimeUnit.SECONDS,
+            checkBundleDiagInfos(config.systemReadyTimeout(), TimeUnit.SECONDS,
                 (timeInfo, bundleDiagInfos) -> {
                     LOG.info("checkBundleDiagInfos: Elapsed time {}s, remaining time {}s, {}",
                         timeInfo.getElapsedTimeInMS() / 1000, timeInfo.getRemainingTimeInMS() / 1000,
@@ -104,8 +111,74 @@ public final class KarafSystemReady extends SimpleSystemReadyMonitor {
         ready();
     }
 
-    @VisibleForTesting
-    DelegatingSystemReadyMonitorMXBean getMbean() {
-        return mbean;
+    /**
+     * Does something similar to Karaf's "diag" CLI command, and throws a {@link SystemStateFailureException} if
+     * anything including bundle wiring is not OK.
+     *
+     * <p>The implementation is based on Karaf's BundleService, and not the BundleStateService, because each Karaf
+     * supported DI system (such as Blueprint and Declarative Services, see String constants in BundleStateService),
+     * will have a separate BundleStateService.  The BundleService however will contain the combined status of all
+     * BundleStateServices.
+     *
+     * @param timeout maximum time to wait for bundles to settle
+     * @param timeoutUnit time unit of timeout
+     * @throws SystemStateFailureException if all bundles do not settle within the timeout period
+     */
+    private void checkBundleDiagInfos(long timeout, TimeUnit timeoutUnit,
+            BiConsumer<TimeInfo, BundleDiagInfos> awaitingListener) throws SystemStateFailureException {
+        LOG.info("checkBundleDiagInfos() started...");
+
+        var timeoutNanos = timeoutUnit.toNanos(timeout);
+        var started = System.nanoTime();
+
+        while (true) {
+            var elapsedNanos = System.nanoTime() - started;
+            var remainingNanos = timeoutNanos - elapsedNanos;
+            var diag = diagProvider.currentDiag();
+            var bundleInfos = BundleDiagInfosImpl.ofDiag(diag);
+
+            var systemState = bundleInfos.getSystemState();
+            switch (systemState) {
+                case Active -> {
+                    // Inform the developer of the green SystemState.Active
+                    awaitingListener.accept(new TimeInfo(TimeUnit.NANOSECONDS.toMillis(elapsedNanos),
+                        TimeUnit.NANOSECONDS.toMillis(remainingNanos)), bundleInfos);
+                    LOG.info("diag successful; system state active ({})", bundleInfos.getFullDiagnosticText());
+                    return;
+                }
+                case Failure, Stopping -> {
+                    LOG.error("""
+                        diag failure; BundleService reports bundle(s) which failed or are already stopping (details in \
+                        following INFO and ERROR log messages...)""");
+                    diag.logState(LOG);
+                    throw new SystemStateFailureException("diag failed; some bundles failed to start", bundleInfos);
+                }
+
+                // FIXME: a.k.a Booting, but the combination of checkstyle and error-prone requires us to use this ugly
+                //        construct. Reasons:
+                //        - we need either a 'default' or 'case null' for checkstyle
+                //        - using a catch-all 'default' is hated by error-prone
+                //        - using a 'case null' without 'default' results in
+                //          https://github.com/google/error-prone/issues/4721
+                //        So when we have fixed error-prone, we need to go back to 'case Booting' and 'case null'.
+                default -> {
+                    if (remainingNanos <= 0) {
+                        // This typically happens due to bundles still in BundleState GracePeriod or Waiting
+                        LOG.error("""
+                            diag failure; BundleService reports bundle(s) which are still not active (details in \
+                            following INFO and ERROR log messages...)""");
+                        diag.logState(LOG);
+                        throw new SystemStateFailureException("diag timeout; some bundles are still not active:",
+                            bundleInfos);
+                    }
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new SystemStateFailureException("Interrupted waiting for a retry", bundleInfos, e);
+                    }
+                }
+            }
+        }
     }
 }
